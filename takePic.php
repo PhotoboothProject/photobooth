c2d460f2f2dfe0b2548a655d088081132339a0b5
Mark Lubkowitz <mail@mark-lubkowitz.de>
2017-05-28 19:51:23 +0200

    Basic rework of takePic.php

--- takePic.php
+++ takePic.php
@@ -1,17 +1,19 @@
 <?php
 
 require_once('db.php');
-$folder = 'images/';
-$file = md5(time()).'.jpg';
-$filename = $folder.$file;
-$shootimage = shell_exec('sudo gphoto2 --capture-image-and-download --filename='.$filename.' images');
+require_once('config.inc.php');
 
+$file = md5(time()).'.jpg';
+$filename_photo = $config['folders']['images'].'/'.$file;
+$filename_thumb = $config['folders']['thumbs'].'/'.$file;
+
+$shootimage = shell_exec('sudo gphoto2 --capture-image-and-download --filename='.$filename_photo.' images');
 
 if(strpos($shootimage, 'New file is in location') === false) {
 	echo json_encode(array('error' => true));	
 } else {
 	// Scale with avconv
-	$scaleimage = shell_exec('avconv -i '.$filename.' -vf scale=500:-1 thumbs/'.$file);
+	$scaleimage = shell_exec('avconv -i '.$filename_photo.' -vf scale=500:-1 '.$filename_thumb);
 	
 	// Insert into DB file
 	$images[] = $file;
