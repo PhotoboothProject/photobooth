b87b40e9a386cbb116d58bbdffc0b8d28fa8191c
F4bsi <fabian.sitzlach@stud.fh-luebeck.de>
2018-03-24 02:50:19 +0100

    Add possibility to show date in caption

--- config.inc.php
+++ config.inc.php
@@ -13,6 +13,8 @@ $config['use_qr'] = true;
 $config['show_fork'] = true;
 $config['previewFromCam'] = false; // experimental see https://github.com/andreknieriem/photobooth/pull/30
 #$config['file_format'] = 'date'; // comment in to get dateformat images
+$config['show_date'] = false; // only works if file_format = date
+$config['date_format'] = 'd.m.Y - G:i';
 
 // FOLDERS
 // change the folders to whatever you like
--- index.php
+++ index.php
@@ -88,6 +88,11 @@ require_once('db.php');
 					<?php
 					$imagelist = ($config['gallery']['newest_first'] === true) ? array_reverse($images) : $images;
 					foreach($imagelist as $image) {
+						$date;
+						if (isset($config['file_format']) && $config['file_format'] == 'date' && $config['show_date'] == true) {
+							$date = DateTime::createFromFormat('Ymd_His', substr($image, 0, strlen($image) - 4));
+						}
+
 						$filename_photo = $config['folders']['images'] . DIRECTORY_SEPARATOR . $image;
 						$filename_thumb = $config['folders']['thumbs'] . DIRECTORY_SEPARATOR . $image;
 
@@ -96,7 +101,7 @@ require_once('db.php');
 
 						echo '<a href="'.DIRECTORY_SEPARATOR.$filename_photo.'" data-size="'.$imageinfo[0].'x'.$imageinfo[1].'" data-med="'.DIRECTORY_SEPARATOR.$filename_thumb.'" data-med-size="'.$imageinfoThumb[0].'x'.$imageinfoThumb[1].'">
 								<img src="'.DIRECTORY_SEPARATOR.$filename_thumb .'" />
-								<figure>Caption</figure>
+								<figure>' . ($date == false ? '' : '<i class="fa fa-clock-o"></i> ' . $date->format($config['date_format'])) . '</figure>
 							</a>';
 					}
 					?>
