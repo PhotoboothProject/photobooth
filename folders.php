6824f63bb88d84394c3b85a6fc5d8e1171a9cc8f
Andreas Blaesius <andi@unlegacy-android.org>
2019-09-13 09:20:38 +0200

    Fix "Undefined variable" PHP Notice / Warning
    PHP Notice: Undefined variable: date in /var/www/html/index.php on line 166, referer: http://127.0.0.1/
    PHP Notice: Undefined variable: config in /var/www/html/folders.php on line 4, referer: http://127.0.0.1/
    PHP Warning: Invalid argument supplied for foreach() in /var/www/html/folders.php on line 4, referer: http://127.0.0.1/
    
    https://github.com/andreknieriem/photobooth/issues/71

--- folders.php
+++ folders.php
@@ -1,6 +1,13 @@
 <?php
 // DON'T MODIFY
 // preparation
+$my_config = 'my.config.inc.php';
+if (file_exists($my_config)) {
+	require_once('my.config.inc.php');
+} else {
+        require_once('config.inc.php');
+}
+
 foreach($config['folders'] as $directory) {
 	if(!is_dir($directory)){
 		mkdir($directory, 0777);
--- index.php
+++ index.php
@@ -150,7 +150,7 @@ require_once('db.php');
 						echo '<h1 style="text-align:center" data-l10n="gallery_no_image"></h1>';
 					} else {
 						foreach($imagelist as $image) {
-							$date;
+							$date = false;
 							if ($config['file_format_date'] == true && $config['show_date'] == true) {
 								$date = DateTime::createFromFormat('Ymd_His', substr($image, 0, strlen($image) - 4));
 							}
