133d9774ba9a75c4011e59713205973e8e2416e1
sualko <klaus@jsxc.org>
2019-09-24 15:06:21 +0200

    fix sass watch task

--- resources/gulpfile.js
+++ resources/gulpfile.js
@@ -10,5 +10,5 @@ gulp.task('sass', function () {
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./sass/**/*.scss', gulp.series('sass'));
 });
