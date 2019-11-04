ade621b939342c17f2872584bf3728cc54284268
Andreas Blaesius <andi@unlegacy-android.org>
2019-11-04 20:06:19 +0100

    Switch to NGINX by default
    Thanks to rawbertp.
    Fix https://github.com/andreknieriem/photobooth/issues/177
    
    Change-Id: I634d23ccbaceb582bcef688e09b081960fcd5f02

--- install-raspbian.sh
+++ install-raspbian.sh
@@ -67,7 +67,7 @@ apt update
 apt dist-upgrade -y
 
 info "### Photobooth needs some software to run."
-apt install -y libapache2-mod-php php-gd gphoto2 unclutter
+apt install -y nginx php-fpm php-gd gphoto2 unclutter
 
 cd /var/www/
 rm -rf html
@@ -112,6 +112,26 @@ else
         xargs curl -L --output /tmp/photobooth-latest.tar.gz
 
     tar -xzvf /tmp/photobooth-latest.tar.gz -C /var/www/html/
+fi
+
+nginx_conf="/etc/nginx/sites-enabled/default"
+
+if [ -f "${nginx_conf}" ]; then
+    info "### Enable PHP in NGINX"
+    cp "${nginx_conf}" ~/nginx-default.bak
+    sed -i 's/^\(\s*\)index index\.html\(.*\)/\1index index\.php index\.html\2/g' "${nginx_conf}"
+    sed -i '/location ~ \\.php$ {/s/^\(\s*\)#/\1/g' "${nginx_conf}"
+    sed -i '/include snippets\/fastcgi-php.conf/s/^\(\s*\)#/\1/g' "${nginx_conf}"
+    sed -i '/fastcgi_pass unix:\/run\/php\//s/^\(\s*\)#/\1/g' "${nginx_conf}"
+    sed -i '/.*fastcgi_pass unix:\/run\/php\//,// { /}/s/^\(\s*\)#/\1/g; }' "${nginx_conf}"
+
+    info "### Testing NGINX config"
+    /usr/sbin/nginx -t -c /etc/nginx/nginx.conf
+
+    info "### Restarting NGINX"
+    systemctl reload nginx
+else
+    error "Can not find ${nginx_conf} !"
 fi
 
 info "### Setting permissions."
