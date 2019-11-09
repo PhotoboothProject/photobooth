a05d4467c318f1320b1776ed4400b64089a60e26
Andreas Blaesius <andi@unlegacy-android.org>
2019-11-09 09:33:47 +0100

    api/download: $image already contains ".jpg"
    Change-Id: I63f0aef95886bb3f512c36e3700306acd8fd4db4

--- api/download.php
+++ api/download.php
@@ -3,7 +3,7 @@
 $image = (isset($_GET['image']) && $_GET['image']) != '' ? $_GET['image'] : false;
 if($image) {
 	header('Content-Type: application/octet-stream');
-	header('Content-Disposition: attachment; filename="photobooth-'.$image.'.jpg"');
+	header('Content-Disposition: attachment; filename="photobooth-'.$image.'"');
 	echo file_get_contents(__DIR__.'/../data/images/'.$image);
 	exit;
 }
