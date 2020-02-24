4ff26cff0f66f52333db4663ed27e14025a30e5e
Andreas Blaesius <andi@unlegacy-android.org>
2020-02-24 19:35:35 +0100

    Download: respect folder settings

--- api/download.php
+++ api/download.php
@@ -1,9 +1,12 @@
 <?php
+require_once('../lib/config.php');
 
 $image = (isset($_GET['image']) && $_GET['image']) != '' ? $_GET['image'] : false;
 if($image) {
+	$filename_source = $config['folders']['images'] . DIRECTORY_SEPARATOR . $image;
+
 	header('Content-Type: application/octet-stream');
 	header('Content-Disposition: attachment; filename="photobooth-'.$image.'"');
-	echo file_get_contents(__DIR__.'/../data/images/'.$image);
+	echo file_get_contents(__DIR__.'/../'.$filename_source);
 	exit;
 }
