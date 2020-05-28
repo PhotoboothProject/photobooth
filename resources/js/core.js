65587a79773ef91ace0266eca0aae02d7cb68bc5
Andreas Blaesius <andi@unlegacy-android.org>
2020-05-28 17:50:12 +0200

    Remove focus on "New Picture" and "New Collage" buttons
    Change-Id: I8a0108cbff6569c27c88b9433d6cbcf796ebe8b9

--- resources/js/core.js
+++ resources/js/core.js
@@ -536,6 +536,7 @@ const photoBooth = (function () {
         e.preventDefault();
 
         public.thrill('photo');
+        $('.newpic').blur();
     });
 
     // Take Collage Button
@@ -543,6 +544,7 @@ const photoBooth = (function () {
         e.preventDefault();
 
         public.thrill('collage');
+        $('.newcollage').blur();
     });
 
     $('#mySidenav .closebtn').on('click', function (e) {
