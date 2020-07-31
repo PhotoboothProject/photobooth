df8bff4568025d665ee890d378cc6defbd838976
Andreas Remdt <me@andreasremdt.com>
2020-07-31 19:48:40 +0200

    Add Babel to Gulp
    Installs Babel and @babel/env to transpile JavaScript into ES5 code
    Adapts the Gulp pipeline to process JavaScript

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,25 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var babel = require('gulp-babel');
 
 gulp.task('sass', function () {
-  return gulp.src('./resources/sass/**/*.scss')
+  return gulp
+    .src('./src/sass/**/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(gulp.dest('./resources/css'));
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./resources/sass/**/*.scss', gulp.series('sass'));
+gulp.task('js', function () {
+  return gulp
+    .src('./src/js/**/*.js')
+    .pipe(babel({ presets: ['@babel/env'] }))
+    .pipe(gulp.dest('./resources/js'));
 });
+
+gulp.task('watch', function () {
+  gulp.watch('./src/sass/**/*.scss', gulp.series('sass'));
+  gulp.watch('./src/js/*.js', gulp.series('js'));
+});
+
+gulp.task('default', gulp.parallel('sass', 'js'));
