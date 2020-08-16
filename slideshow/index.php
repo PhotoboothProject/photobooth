c9ec0d3dcc02a0b279a91fd7b30243fa10c050a9
Andreas Blaesius <andi@unlegacy-android.org>
2020-08-16 20:29:05 +0200

    standalone slideshow: only use pictures if we can read them
    Change-Id: Ib4e91a0463d1be704fcacc5432c8970fc836816b

--- slideshow/index.php
+++ slideshow/index.php
@@ -70,6 +70,7 @@ $imagelist = array_reverse($images);
 			$filename_photo = '../' . $config['folders']['images'] . DIRECTORY_SEPARATOR . $image;
 		}
 
+		if (is_readable($filename_photo)) {
 		?>
 		<div class="center">
 			<figure>
@@ -77,7 +78,7 @@ $imagelist = array_reverse($images);
 				<figcaption><?=$date?></figcaption>
 			</figure>
 		</div>
-
+		<?php } ?>
 		<?php endforeach; ?>
 		<?php endif; ?>
 	</div>
