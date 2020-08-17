5c95473e88379c32609210eccaebff18ef8960f7
Andreas Blaesius <andi@unlegacy-android.org>
2020-08-17 10:19:23 +0000

    applyEffects: check for supported file type
    Change-Id: I7d292ddf752bc6284caca4844636fe7feda7c269

--- api/applyEffects.php
+++ api/applyEffects.php
@@ -8,9 +8,9 @@ require_once('../lib/polaroid.php');
 require_once('../lib/resize.php');
 require_once('../lib/collage.php');
 
-if (empty($_POST['file']) || !preg_match('/^[a-z0-9_]+\.jpg$/', $_POST['file'])) {
+if (empty($_POST['file'])) {
     die(json_encode([
-        'error' => 'Invalid file provided',
+        'error' => 'No file provided',
     ]));
 }
 
@@ -48,6 +48,15 @@ if (isset($_POST['isCollage']) && $_POST['isCollage'] === 'true') {
 if (!file_exists($filename_tmp)) {
     die(json_encode([
         'error' => 'File does not exist'
+    ]));
+}
+
+// Only jpg/jpeg are supported
+$imginfo = getimagesize($filename_tmp);
+$mimetype = $imginfo['mime'];
+if ($mimetype != 'image/jpg' && $mimetype != 'image/jpeg') {
+    die(json_encode([
+        'error' => 'The source file type ' . $mimetype . ' is not supported'
     ]));
 }
 
