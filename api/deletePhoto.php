8051325b00e186057dcf4596308f568fb72889ee
Andreas Blaesius <andi@unlegacy-android.org>
2020-08-17 10:27:37 +0000

    deletePhoto: check for supported file type

--- api/deletePhoto.php
+++ api/deletePhoto.php
@@ -4,15 +4,24 @@ header('Content-Type: application/json');
 require_once('../lib/db.php');
 require_once('../lib/config.php');
 
-if (empty($_POST['file']) || !preg_match('/^[a-z0-9_]+\.jpg$/', $_POST['file'])) {
+if (empty($_POST['file'])) {
     die(json_encode([
-        'error' => 'No or invalid file provided',
+        'error' => 'No file provided'
     ]));
 }
 
 $file = $_POST['file'];
 $filePath = $config['foldersAbs']['images'] . DIRECTORY_SEPARATOR . $file;
 $filePathThumb = $config['foldersAbs']['thumbs'] . DIRECTORY_SEPARATOR . $file;
+
+// Only jpg/jpeg are supported
+$imginfo = getimagesize($filePath);
+$mimetype = $imginfo['mime'];
+if ($mimetype != 'image/jpg' && $mimetype != 'image/jpeg') {
+    die(json_encode([
+        'error' => 'The source file type ' . $mimetype . ' is not supported'
+    ]));
+}
 
 if (!unlink($filePath) || !unlink($filePathThumb)) {
     die(json_encode([
