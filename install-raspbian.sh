9ae33430853e8621f00f69305d3f1c913cab563b
Andreas Blaesius <andi@unlegacy-android.org>
2020-09-08 17:36:46 +0200

    install: remove unneeded "sudo" on yarn installation

--- install-raspbian.sh
+++ install-raspbian.sh
@@ -169,7 +169,7 @@ for package in "${COMMON_PACKAGES[@]}"; do
         info "[Package]   Installing missing common package: ${package}"
         if [[ ${package} == "yarn" ]]; then
                 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
-                echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+                echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
                 apt update
         fi
         apt install -y ${package}
