5e970b2e9d12349374c3c327ea01634f445776db
Andreas Blaesius <andi@unlegacy-android.org>
2020-09-27 09:34:58 +0200

    api (fix): only try to delete tmp image if keep images is enabled
    Change-Id: Ib4bd8d63a2e64453429a99698f6b3a0315a7ff73

--- api/deletePhoto.php
+++ api/deletePhoto.php
@@ -41,11 +41,13 @@ if ($config['chroma_keying']) {
     }
 }
 
-if (is_readable($filePathTmp)) {
-    if (!unlink ($filePathTmp)) {
-        die(json_encode([
-            'error' => 'Could not delete tmp file',
-        ]));
+if ($config['keep_images']) {
+    if (is_readable($filePathTmp)) {
+        if (!unlink ($filePathTmp)) {
+            die(json_encode([
+                'error' => 'Could not delete tmp file',
+            ]));
+        }
     }
 }
 
