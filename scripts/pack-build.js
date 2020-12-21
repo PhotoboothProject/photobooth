168bd088655390c0d67dfc41e8342d6afeb19c99
jacques42 <github.com@weismantel.de>
2020-12-21 22:19:21 +0100

    Updated pack-build.js

--- scripts/pack-build.js
+++ scripts/pack-build.js
@@ -99,6 +99,7 @@ function createArchive(fileName, archive) {
     archive.directory('node_modules/component-emitter');
     archive.directory('node_modules/component-inherit');
     archive.directory('node_modules/cookie');
+    archive.directory('node_modules/core-js');
     archive.directory('node_modules/debug');
     archive.directory('node_modules/engine.io');
     archive.directory('node_modules/engine.io-client');
@@ -115,6 +116,7 @@ function createArchive(fileName, archive) {
     archive.directory('node_modules/object-component');
     archive.directory('node_modules/parseqs');
     archive.directory('node_modules/parseuri');
+    archive.directory('node_modules/regenerator-runtime');
     archive.directory('node_modules/rpio');
     archive.directory('node_modules/socket.io');
     archive.directory('node_modules/socket.io-adapter');
