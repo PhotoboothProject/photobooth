PhotoboothProject/photobooth

commit 77a7dcde04c0f9fef90e183541696d4fd9f47269

feature (gallery): add button to delete an image, enable by default
Change-Id: I7e1fc1d82521789378028d98b040994083f51e96

lib/db.php
@@ -26,8 +26,6 @@ function deleteImageFromDB($filename) {
 	$images = getImagesFromDB();
 
 	unset($images[array_search($filename, $images)]);
-
-	file_put_contents(DB_FILE, json_encode($images));
 }
 
 function isImageInDB($filename) {

config/config.inc.php
@@ -44,6 +44,7 @@ $config['use_filter'] = true;
 $config['default_imagefilter'] = 'plain';
 $config['disabled_filters'] = array();
 $config['allow_delete'] = true;
+$config['allow_delete_from_gallery'] = true;
 $config['polaroid_effect'] = false;
 $config['polaroid_rotation'] = '0';
 $config['take_frame'] = false;

lib/configsetup.inc.php
@@ -323,6 +323,11 @@ $configsetup = [
 			'type' => 'checkbox',
 			'name' => 'allow_delete',
 			'value' => $config['allow_delete']
+		],
+		'allow_delete_from_gallery' => [
+			'type' => 'checkbox',
+			'name' => 'allow_delete_from_gallery',
+			'value' => $config['allow_delete_from_gallery']
 		]
 	],
 	'jpeg_quality' => [

src/js/photoinit.js
@@ -1,5 +1,5 @@
 /* exported initPhotoSwipeFromDOM */
-/* global photoBooth */
+/* global photoBooth i18n */
 function initPhotoSwipeFromDOM(gallerySelector) {
     let gallery,
         ssRunning = false,
@@ -176,6 +176,31 @@ function initPhotoSwipeFromDOM(gallerySelector) {
 
         gallery.init();
     };
+
+    // Delete from DB in gallery
+    $('.pswp__button--delete').on('click', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        let img = gallery.currItem.src;
+        img = img.split('\\').pop().split('/').pop();
+
+        const msg = i18n('really_delete_image');
+        const really = confirm(img + ' ' + msg);
+        if (really) {
+            photoBooth.deleteImage(img, (data) => {
+                if (data.success) {
+                    console.log('Deleted ' + img);
+                    photoBooth.reloadPage();
+                } else {
+                    console.log('Error while deleting ' + img);
+                    setTimeout(function () {
+                        photoBooth.reloadPage();
+                    }, 5000);
+                }
+            });
+        }
+    });
 
     // QR in gallery
     $('.pswp__button--qrcode').on('click touchstart', function (e) {

src/sass/photoswipe-bottom.scss
@@ -8,7 +8,8 @@
 .pswp__button--playpause,
 .pswp__button--share,
 .pswp__button--zoom,
-.pswp__button--fs {
+.pswp__button--fs,
+.pswp__button--delete {
   left: 40%;
 }
 
@@ -63,7 +64,8 @@
   .pswp__button--playpause,
   .pswp__button--share,
   .pswp__button--zoom,
-  .pswp__button--fs {
+  .pswp__button--fs,
+  .pswp__button--delete {
     width: calc(2 * 1em);
     height: calc(2 * 1em);
     line-height: calc(2 * 1em);

src/sass/vendor/_photoswipe.scss
@@ -7,7 +7,8 @@
 .pswp__button--playpause,
 .pswp__button--share,
 .pswp__button--zoom,
-.pswp__button--fs {
+.pswp__button--fs,
+.pswp__button--delete {
   color: $galleryButtonColor;
   background: transparent !important;
   font-size: 1.625em;

template/pswp.template.php
@@ -53,6 +53,10 @@
                 <button type="button" class="pswp__button pswp__button--playpause fa fa-play" title="Play Slideshow"></button>
                 <?php endif; ?>
 
+                <?php if ($config['allow_delete_from_gallery']): ?>
+                <button type="button" class="pswp__button pswp__button--delete" title="Delete"><i class="fa fa-trash"></i></button>
+                <?php endif; ?>
+
                 <!-- Preloader demo http://codepen.io/dimsemenov/pen/yyBWoR -->
                 <!-- element will get class pswp__preloader--active when preloader is running -->
                 <div class="pswp__preloader">

commit 0df5426af2d00f69d7408a6612cc25e3a155ba79

fix (db): fix removing deleted images from database
- issue was introduced in commit https://github.com/andi34/photobooth/commit/77a7dcde04c0f9fef90e183541696d4fd9f47269

Change-Id: I1b7337432f5ca6dc7d7b7a2b7da54a4c96e9dc1f

lib/db.php
@@ -26,6 +26,7 @@ function deleteImageFromDB($filename) {
 	$images = getImagesFromDB();
 
 	unset($images[array_search($filename, $images)]);
+	file_put_contents(DB_FILE, json_encode($images));
 
 	if (file_exists(DB_FILE) && empty($images)) {
 		unlink(DB_FILE);