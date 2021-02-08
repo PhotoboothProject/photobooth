1dfdc9d64de1c5215f4d5e58c5278fa57b768c2b
Andreas Blaesius <andi@unlegacy-android.org>
2021-02-08 19:41:49 +0100

    install-raspbian: only allow git version installation for now
    Change-Id: I43de8f9fa320ced4aac3fcb30005f50f6599c679

--- install-raspbian.sh
+++ install-raspbian.sh
@@ -201,54 +201,35 @@ else
     INSTALLFOLDERPATH="/var/www/html/$INSTALLFOLDER/"
 fi
 
-echo -e "\033[0;33m### Do you like to install from git? This will take more"
-read -p "### time but it is recommended and makes updating Photobooth easier. [y/N] " -n 1 -r
+info "### Now we are going to install Photobooth."
+git clone https://github.com/andi34/photobooth $INSTALLFOLDER
+cd $INSTALLFOLDERPATH
+
+echo -e "\033[0;33m### Please select a version to install:"
+echo -e "    1 Install last development version"
+echo -e "    2 Install last stable Release"
+read -p "Please enter your choice: " -n 1 -r
 echo -e "\033[0m"
-if [[ $REPLY =~ ^[Yy]$ ]]
+if [[ $REPLY =~ ^[1]$ ]]
 then
-    info "### Your wish is my command!"
-
-    info "### Now we are going to install Photobooth."
-    git clone https://github.com/andi34/photobooth $INSTALLFOLDER
-    cd $INSTALLFOLDERPATH
-
-    echo -e "\033[0;33m### Please select a version to install:"
-    echo -e "    1 Install last development version"
-    echo -e "    2 Install last stable Release"
-    read -p "Please enter your choice: " -n 1 -r
-    echo -e "\033[0m"
-    if [[ $REPLY =~ ^[1]$ ]]
-    then
-      info "### We are installing last development version"
-      git fetch origin dev
-      git checkout origin/dev
-    else
-      if [[ ! $REPLY =~ ^[2]$ ]]
-        then
-        info "### Invalid choice!"
-      fi
-      LATEST_VERSION=$( git describe --tags `git rev-list --tags --max-count=1` )
-      info "### We are installing last stable Release: Version $LATEST_VERSION"
-      git checkout $LATEST_VERSION
-    fi
-
-    git submodule update --init
-
-    info "### Get yourself a hot beverage. The following step can take up to 15 minutes."
-    yarn install
-    yarn build
+  info "### We are installing last development version"
+  git fetch origin dev
+  git checkout origin/dev
 else
-    info "### Your wish is my command!"
-
-    info "### Downloading the latest release and extracting it."
-    curl -s https://api.github.com/repos/andi34/photobooth/releases/latest |
-        jq '.assets[].browser_download_url | select(endswith(".tar.gz"))' |
-        xargs curl -L --output /tmp/photobooth-latest.tar.gz
-
-    mkdir -p $INSTALLFOLDERPATH
-    tar -xzvf /tmp/photobooth-latest.tar.gz -C $INSTALLFOLDERPATH
-    cd $INSTALLFOLDERPATH
+  if [[ ! $REPLY =~ ^[2]$ ]]
+    then
+    info "### Invalid choice!"
+  fi
+  LATEST_VERSION=$( git describe --tags `git rev-list --tags --max-count=1` )
+  info "### We are installing last stable Release: Version $LATEST_VERSION"
+  git checkout $LATEST_VERSION
 fi
+
+git submodule update --init
+
+info "### Get yourself a hot beverage. The following step can take up to 15 minutes."
+yarn install
+yarn build
 
 echo -e "\033[0;33m### Do you like to use a Raspberry Pi (HQ) Camera to take pictures?"
 read -p "### If yes, this will generate a personal configuration with all needed changes. [y/N] " -n 1 -r
