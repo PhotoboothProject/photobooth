d0d0d05a92b33f4406a9abd38bbb653e12a1bab0
Andreas Blaesius <andi@unlegacy-android.org>
2021-03-25 20:16:54 +0100

    lib (config): fix "Undefined index: ui" notice
    Change-Id: I46d0d75d3b2eddea69f961629d0b86e574079c41

--- lib/config.php
+++ lib/config.php
@@ -85,8 +85,6 @@ $mailTemplates = [
 
 require_once $default_config_file;
 
-$config['mail']['subject'] = $mailTemplates[$config['ui']['language']]['mail']['subject'];
-$config['mail']['text'] = $mailTemplates[$config['ui']['language']]['mail']['text'];
 $config['take_picture']['cmd'] = $cmds[$os]['take_picture']['cmd'];
 $config['take_picture']['msg'] = $cmds[$os]['take_picture']['msg'];
 $config['print']['cmd'] = $cmds[$os]['print']['cmd'];
@@ -112,11 +110,20 @@ if (file_exists($my_config_file)) {
     require_once $my_config_file;
 
     if (!isset($config['mail']['subject'])) {
-        $config['mail']['subject'] = $mailTemplates[$config['ui']['language']]['mail']['subject'];
+        if (isset($config['ui']['language'])) {
+            $config['mail']['subject'] = $mailTemplates[$config['ui']['language']]['mail']['subject'];
+        } else {
+            $config['mail']['subject'] = $mailTemplates[$defaultConfig['ui']['language']]['mail']['subject'];
+        }
     }
     if (!isset($config['mail']['text'])) {
-        $config['mail']['text'] = $mailTemplates[$config['ui']['language']]['mail']['text'];
+        if (isset($config['ui']['language'])) {
+            $config['mail']['text'] = $mailTemplates[$config['ui']['language']]['mail']['text'];
+        } else {
+            $config['mail']['text'] = $mailTemplates[$defaultConfig['ui']['language']]['mail']['text'];
+        }
     }
+
     $config = array_deep_merge($defaultConfig, $config);
 }
 
