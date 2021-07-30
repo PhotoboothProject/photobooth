9186b0debb0f80b3b1d0abd38a24210a49f14bc9
jacques42 <github.com@weismantel.de>
2021-07-30 12:49:49 +0200

    debugpanel: small improvements and added access to gphoto logs (PR #277)

--- api/serverInfo.php
+++ api/serverInfo.php
@@ -9,11 +9,11 @@ $content = $_GET['content'];
 
 switch ($content) {
     case 'nav-remotebuzzerlog':
-        echo dumpfile($config['foldersAbs']['tmp'] . '/' . $config['remotebuzzer']['logfile']);
+        echo dumpfile($config['foldersAbs']['tmp'] . '/' . $config['remotebuzzer']['logfile'], true);
         break;
 
     case 'nav-synctodrivelog':
-        echo dumpfile($config['foldersAbs']['tmp'] . '/' . $config['synctodrive']['logfile']);
+        echo dumpfile($config['foldersAbs']['tmp'] . '/' . $config['synctodrive']['logfile'], true);
         break;
 
     case 'nav-myconfig':
@@ -25,15 +25,25 @@ switch ($content) {
         break;
 
     case 'nav-bootconfig':
-        echo dumpfile('/boot/config.txt');
+        echo dumpfile('/boot/config.txt', null);
+        break;
+
+    case 'nav-cameralog':
+        echo dumpfile($config['foldersAbs']['tmp'] . '/' . $config['take_picture']['logfile'], false);
         break;
 
     default:
-        echo 'UNKNOWN COMMAND';
+        echo 'Unknown debug panel parameter';
         break;
 }
 
-function dumpfile($file) {
+function dumpfile($file, $devModeRequired) {
+    global $config;
+
+    if ($devModeRequired !== null && $config['dev']['enabled'] !== $devModeRequired) {
+        return 'INFO: Dev mode is ' . ($config['dev']['enabled'] ? 'enabled - please disable' : 'disabled - please enable') . ' to see logs';
+    }
+
     if (!file_exists($file)) {
         return 'INFO: File (' . $file . ') does not exist';
     } elseif (!is_file($file)) {
