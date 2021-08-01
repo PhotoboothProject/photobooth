f27d5d93fc7069ce26e28b4133a5429eec93fa99
jacques42 <github.com@weismantel.de>
2021-08-01 23:40:48 +0200

    remotebuzzer: config migration use install path

--- install-raspbian.sh
+++ install-raspbian.sh
@@ -358,7 +358,7 @@ www-data ALL=(ALL) NOPASSWD: /sbin/shutdown
 EOF
 
 # update artifacts in user configuration from old remotebuzzer implementation
-sed -i '/remotebuzzer/{n;n;s/enabled/usebuttons/}' config/my.config.inc.php
+sed -i '/remotebuzzer/{n;n;s/enabled/usebuttons/}' $INSTALLFOLDERPATH/config/my.config.inc.php
 fi
 # remotebuzzer config depending on version end
 
