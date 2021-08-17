3f5d3abaf532cd0885f4916e4e169588113bb29e
Andreas Blaesius <andi@unlegacy-android.org>
2021-08-17 21:25:58 +0200

    install-rapsbian / photobooth.desktop: adjust chromium flags
    List of Chromium Command Line Switches:
    https://peter.sh/experiments/chromium-command-line-switches/
    
    Change-Id: I21cbac69492f7681289614820d9481eb8d69b2b6

--- install-raspbian.sh
+++ install-raspbian.sh
@@ -325,7 +325,7 @@ then
 @xset s off
 @xset -dpms
 @xset s noblank
-@chromium-browser --incognito --kiosk http://localhost/
+@chromium-browser --noerrdialogs --disable-infobars --disable-translate --no-first-run --check-for-update-interval=31536000 --use-fake-ui-for-media-stream --start-fullscreen --kiosk http://127.0.0.1 --incognito --touch-events=enabled
 
 @unclutter -idle 3
 
