222e7c35d2cb6ee98bfbd180624b7b768a052b4a
Andreas Blaesius <andi@unlegacy-android.org>
2021-08-24 10:27:38 +0200

    Updater: allow using overrides.css

--- update.php
+++ update.php
@@ -47,6 +47,9 @@ if (
 	<?php if ($config['ui']['rounded_corners'] && $config['ui']['style'] === 'classic'): ?>
 	<link rel="stylesheet" href="resources/css/rounded.css" />
 	<?php endif; ?>
+	<?php if (is_file("private/overrides.css")): ?>
+	<link rel="stylesheet" href="private/overrides.css" />
+	<?php endif; ?>
 </head>
 
 <body class="updatewrapper">
