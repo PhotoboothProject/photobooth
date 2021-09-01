dc81679820ecf87d82a4aaea6c6ee3baecf91c6d
Andreas Blaesius <andi@unlegacy-android.org>
2021-09-01 10:36:44 +0200

    welcome.php: fix node_modules path

--- welcome.php
+++ welcome.php
@@ -86,7 +86,7 @@ if ($os == 'linux') {
 	</div>
 
 	<script src="node_modules/whatwg-fetch/dist/fetch.umd.js"></script>
-	<script type="text/javascript" src="../node_modules/jquery/dist/jquery.min.js"></script>
+	<script type="text/javascript" src="node_modules/jquery/dist/jquery.min.js"></script>
 	<script type="text/javascript" src="api/config.php"></script>
 	<script type="text/javascript" src="resources/js/theme.js"></script>
 </body>
