2a94ba55d0a7cbce697a1f2343107676b0284538
Andreas Blaesius <andi@unlegacy-android.org>
2021-09-24 12:06:47 +0200

    fix (dependencies): we need photoboothTools
    Change-Id: I29d82c9987e0d8bf3a712a445191a4b65a4104bd

--- dependencies.php
+++ dependencies.php
@@ -66,6 +66,7 @@ if (
 
 	<script type="text/javascript" src="node_modules/jquery/dist/jquery.min.js"></script>
 	<script type="text/javascript" src="api/config.php"></script>
+	<script type="text/javascript" src="resources/js/tools.js"></script>
 	<script type="text/javascript" src="resources/js/theme.js"></script>
 	<script type="text/javascript" src="resources/js/dependencies.js"></script>
 	<script src="node_modules/whatwg-fetch/dist/fetch.umd.js"></script>
