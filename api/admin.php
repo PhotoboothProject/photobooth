c993241889ca2b2b4d24e4a56727a00590b35306
F4bsi <27418829+F4bsi@users.noreply.github.com>
2021-09-26 01:04:25 +0200

    Fix Typo in admin.php
    When using a custom style and not creating '../resources/css/custom_chromakeying.css' the code tries to copy the default file, which results in an error because the path has duplicate .css and the file cannot be found: 
    
    <br />
    <b>Warning</b>:  copy(../resources/css/modern_chromakeying.css.css): failed to open stream: No such file or directory in <b>/var/www/html/api/admin.php</b> on line <b>117</b><br />
    "success"
    
    This results in the admin ui staying in the "saving" state indefinitely. Also it still returns "success" which might need to be changed.

--- api/admin.php
+++ api/admin.php
@@ -114,7 +114,7 @@ if ($data['type'] == 'config') {
                 copy('../resources/css/modern_style.css', '../resources/css/custom_style.css');
             }
             if (!file_exists('../resources/css/custom_chromakeying.css')) {
-                copy('../resources/css/modern_chromakeying.css.css', '../resources/css/custom_chromakeying.css');
+                copy('../resources/css/modern_chromakeying.css', '../resources/css/custom_chromakeying.css');
             }
             if (!file_exists('../resources/css/custom_live_chromakeying.css')) {
                 copy('../resources/css/modern_live_chromakeying.css', '../resources/css/custom_live_chromakeying.css');
