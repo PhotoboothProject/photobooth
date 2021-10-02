b963cd8675b3d11eb87a807a69be3df556173011
Andreas Blaesius <andi@unlegacy-android.org>
2021-10-02 11:36:14 +0200

    install-raspbian: auto install needed packages
    Change-Id: Ib02ed3aa4b261abecb82c6aa71b7a1e4395f9c1d

--- install-raspbian.sh
+++ install-raspbian.sh
@@ -179,7 +179,7 @@ EOF
 
 kioskbooth_light() {
     info "### Installing needed dependencies..."
-    apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox chromium-browser
+    apt install -y --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox chromium-browser
 
 cat >> /etc/xdg/openbox/autostart <<EOF
 # turn off display power management system
