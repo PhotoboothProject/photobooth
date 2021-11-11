75bd6ba0e2955b12bb05b481e791858b2fdf24cf
Andreas Blaesius <andi@unlegacy-android.org>
2021-11-11 17:30:21 +0100

    build(deps): bump gulp-sass from 4.1.0 to 5.0.0
    Close #264
    
    Change-Id: Ib2aa3d6921f2878e974302524edcc05de858fd6c

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var sass = require('gulp-sass')(require('sass'));
 var babel = require('gulp-babel');
 
 gulp.task('sass', function () {
