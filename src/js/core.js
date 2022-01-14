PhotoboothProject/photobooth

commit 97fbf1c7e9e6ea4bcceaad4f3f5121dfc6625b02

core: remove duplicate code in startVideo, enable gphoto background view
Change-Id: Ib5ec32ccdf8e1c645d9a2cac0f6ba6634c479b9d

src/js/core.js
@@ -128,14 +128,67 @@ const photoBooth = (function () {
         }
     };
 
+    api.getAndDisplayMedia = function (mode, retry = 0) {
+        const getMedia =
+            navigator.mediaDevices.getUserMedia ||
+            navigator.mediaDevices.webkitGetUserMedia ||
+            navigator.mediaDevices.mozGetUserMedia ||
+            false;
+
+        if (!getMedia) {
+            return;
+        }
+
+        if (config.preview.flipHorizontal) {
+            idVideoView.addClass('flip-horizontal');
+            idVideoPreview.addClass('flip-horizontal');
+        }
+
+        getMedia
+            .call(navigator.mediaDevices, webcamConstraints)
+            .then(function (stream) {
+                if (mode === 'preview') {
+                    idVideoPreview.show();
+                    videoPreview.srcObject = stream;
+                    wrapper.css('background-image', 'none');
+                    wrapper.css('background-color', 'transparent');
+                } else {
+                    idVideoView.show();
+                    videoView.srcObject = stream;
+                }
+                api.stream = stream;
+            })
+            .catch(function (error) {
+                photoboothTools.console.log('Could not get user media: ', error);
+                if (config.preview.mode === 'gphoto' && retry < 3) {
+                    retry += 1;
+                    setTimeout(function () {
+                        api.getAndDisplayMedia('preview', retry);
+                    }, retry * 1000);
+                }
+            });
+    };
+
+    api.startWebcam = function () {
+        const dataVideo = {
+            play: 'true'
+        };
+
+        jQuery
+            .post('api/takeVideo.php', dataVideo)
+            .done(function (result) {
+                photoboothTools.console.log('Start webcam', result);
+                pid = result.pid;
+            })
+            .fail(function (xhr, status, result) {
+                photoboothTools.console.log('Could not start webcam', result);
+            });
+    };
+
     api.startVideo = function (mode) {
         if (config.preview.asBackground) {
             api.stopVideo('preview');
         }
-
-        const dataVideo = {
-            play: 'true'
-        };
 
         if (!navigator.mediaDevices) {
             return;
@@ -143,111 +196,18 @@ const photoBooth = (function () {
 
         if (config.preview.mode === 'gphoto') {
             if (!config.preview.gphoto_bsm && mode === 'preview') {
-                jQuery
-                    .post('api/takeVideo.php', dataVideo)
-                    .done(function (result) {
-                        photoboothTools.console.log('Start webcam', result);
-                        pid = result.pid;
-                    })
-                    .fail(function (xhr, status, result) {
-                        photoboothTools.console.log('Could not start webcam', result);
-                    });
+                api.startWebcam();
+                if (config.preview.asBackground) {
+                    api.getAndDisplayMedia('preview');
+                }
             } else if (!config.preview.gphoto_bsm && mode === 'view') {
-                const getMedia =
-                    navigator.mediaDevices.getUserMedia ||
-                    navigator.mediaDevices.webkitGetUserMedia ||
-                    navigator.mediaDevices.mozGetUserMedia ||
-                    false;
-
-                if (!getMedia) {
-                    return;
-                }
-
-                if (config.preview.flipHorizontal) {
-                    idVideoView.addClass('flip-horizontal');
-                    idVideoPreview.addClass('flip-horizontal');
-                }
-
-                getMedia
-                    .call(navigator.mediaDevices, webcamConstraints)
-                    .then(function (stream) {
-                        idVideoView.show();
-                        videoView.srcObject = stream;
-                        api.stream = stream;
-                    })
-                    .catch(function (error) {
-                        photoboothTools.console.log('Could not get user media: ', error);
-                    });
+                api.getAndDisplayMedia('view');
             } else {
-                jQuery
-                    .post('api/takeVideo.php', dataVideo)
-                    .done(function (result) {
-                        photoboothTools.console.log('Start webcam', result);
-                        pid = result.pid;
-                        const getMedia =
-                            navigator.mediaDevices.getUserMedia ||
-                            navigator.mediaDevices.webkitGetUserMedia ||
-                            navigator.mediaDevices.mozGetUserMedia ||
-                            false;
-
-                        if (!getMedia) {
-                            return;
-                        }
-
-                        if (config.preview.flipHorizontal) {
-                            idVideoView.addClass('flip-horizontal');
-                            idVideoPreview.addClass('flip-horizontal');
-                        }
-
-                        getMedia
-                            .call(navigator.mediaDevices, webcamConstraints)
-                            .then(function (stream) {
-                                idVideoView.show();
-                                videoView.srcObject = stream;
-                                api.stream = stream;
-                            })
-                            .catch(function (error) {
-                                photoboothTools.console.log('Could not get user media: ', error);
-                            });
-                    })
-                    .fail(function (xhr, status, result) {
-                        photoboothTools.console.log('Could not start webcam', result);
-                    });
+                api.startWebcam();
+                api.getAndDisplayMedia('view');
             }
         } else {
-            const getMedia =
-                navigator.mediaDevices.getUserMedia ||
-                navigator.mediaDevices.webkitGetUserMedia ||
-                navigator.mediaDevices.mozGetUserMedia ||
-                false;
-
-            if (!getMedia) {
-                return;
-            }
-
-            if (config.preview.flipHorizontal) {
-                idVideoView.addClass('flip-horizontal');
-                idVideoPreview.addClass('flip-horizontal');
-            }
-
-            getMedia
-                .call(navigator.mediaDevices, webcamConstraints)
-                .then(function (stream) {
-                    if (mode === 'preview') {
-                        idVideoPreview.show();
-                        videoPreview.srcObject = stream;
-                        api.stream = stream;
-                        wrapper.css('background-image', 'none');
-                        wrapper.css('background-color', 'transparent');
-                    } else {
-                        idVideoView.show();
-                        videoView.srcObject = stream;
-                    }
-                    api.stream = stream;
-                })
-                .catch(function (error) {
-                    photoboothTools.console.log('Could not get user media: ', error);
-                });
+            api.getAndDisplayMedia('preview');
         }
     };
 

commit a1cdcc0f888fe4757449d057b167a8b40273a2bc

core: fix preview from device cam at countdown
Inside api.startVideo we've only checked for the "view" mode if preview
from gphoto was enabled, else we always only used the "preview" mode.
Because of this a "view" of the device cam was not visible at the countdown.

The bug was introduced in commit 97fbf1c7.

We should consider renaming some vars in future for an easier understanding
while looking at the code.

Change-Id: I5791b870046dc5be4a7dcf0bd1aeda6434137f33

src/js/core.js
@@ -195,20 +195,18 @@ const photoBooth = (function () {
             return;
         }
 
-        if (config.preview.mode === 'gphoto') {
-            if (config.preview.gphoto_bsm) {
+        if (mode === 'preview') {
+            if (config.preview.mode === 'gphoto' && !config.preview.gphoto_bsm) {
                 api.startWebcam();
-                api.getAndDisplayMedia('view');
-            } else if (mode === 'view') {
-                api.getAndDisplayMedia('view');
-            } else {
-                api.startWebcam();
-                if (mode === 'preview' && config.preview.asBackground) {
-                    api.getAndDisplayMedia('preview');
-                }
             }
-        } else {
             api.getAndDisplayMedia('preview');
+        } else if (mode === 'init') {
+            api.startWebcam();
+        } else {
+            if (config.preview.mode === 'gphoto' && config.preview.gphoto_bsm) {
+                api.startWebcam();
+            }
+            api.getAndDisplayMedia('view');
         }
     };
 