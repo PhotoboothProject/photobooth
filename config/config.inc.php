e83d028d1a56304d62d6532f097653c06e44a3a6
Andreas Blaesius <andi@unlegacy-android.org>
2022-05-05 19:30:51 +0200

    config (collage): adjust default collage layout config
    - use new layout naming
    - adjust possible values
    
    Change-Id: I3d6e5f80569065d8e9c89ad877eec12aa25e7932

--- config/config.inc.php
+++ config/config.inc.php
@@ -94,8 +94,8 @@ $config['collage']['only'] = false;
 $config['collage']['cntdwn_time'] = '3';
 $config['collage']['continuous'] = true;
 $config['collage']['continuous_time'] = '5';
-// possible layout values: '2x2', '2x2-2', '2x4', '2x4-2', '1+3', '1+3-2', '3+1', '1+2', '2+1'
-$config['collage']['layout'] = '2x2-2';
+// possible layout values: '2+2', '2+2-2', '1+3', '1+3-2', '3+1', '1+2', '2+1', '2x4', '2x4-2'
+$config['collage']['layout'] = '2+2-2';
 $config['collage']['dashedline_color'] = '#000000';
 $config['collage']['keep_single_images'] = false;
 // specify key id (e.g. 13 is the enter key) to use that key to take a collage (collage key)
