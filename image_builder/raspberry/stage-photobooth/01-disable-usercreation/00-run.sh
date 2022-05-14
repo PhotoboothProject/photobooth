bacdb0ffc7b3f11a3a26141a79983995ad42965c
Andreas Blaesius <andi@unlegacy-android.org>
2022-05-14 12:55:57 +0200

    image_builder(raspberry): Disable raspiconfig at boot

--- image_builder/raspberry/stage-photobooth/01-disable-usercreation/00-run.sh
+++ image_builder/raspberry/stage-photobooth/01-disable-usercreation/00-run.sh
@@ -1,5 +1,16 @@
 #!/bin/bash -e
 
+# Disable raspiconfig at boot
+on_chroot << EOF
+if [ -e /etc/profile.d/raspi-config.sh ]; then
+    rm -f /etc/profile.d/raspi-config.sh
+    if [ -e /etc/systemd/system/getty@tty1.service.d/raspi-config-override.conf ]; then
+        rm /etc/systemd/system/getty@tty1.service.d/raspi-config-override.conf
+    fi
+    telinit q
+fi
+EOF
+
 # Disable users creation on first boot
 on_chroot << EOF
 systemctl stop userconfig
