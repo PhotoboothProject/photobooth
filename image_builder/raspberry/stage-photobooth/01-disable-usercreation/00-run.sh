c5c2e672f6a462a9799e37c84c3311d189b2363a
Andreas Blaesius <andi@unlegacy-android.org>
2022-05-16 11:24:29 +0200

    image_builder(raspberry): don't start Pi Wizard on first boot

--- image_builder/raspberry/stage-photobooth/01-disable-usercreation/00-run.sh
+++ image_builder/raspberry/stage-photobooth/01-disable-usercreation/00-run.sh
@@ -16,5 +16,9 @@ on_chroot << EOF
 systemctl stop userconfig
 systemctl disable userconfig
 systemctl mask userconfig
+
+if [ -e /etc/xdg/autostart/piwiz.desktop ]; then
+    rm -f /etc/xdg/autostart/piwiz.desktop
+fi
 EOF
 
