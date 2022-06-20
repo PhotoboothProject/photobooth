d46f21c174930f506c15c1b74a978a6c8677e800
Andreas Blaesius <andi@unlegacy-android.org>
2022-06-20 20:24:22 +0200

    template(classic): change take picture and take collage order
    Change-Id: If74eda5d243de69c6775d84a4b73ecf5cfafb0f5

--- template/classic.template.php
+++ template/classic.template.php
@@ -55,14 +55,15 @@
 						<span data-i18n="use_button"></span>
 				</div>
 				<?php else: ?>
+					<?php if (!($config['collage']['enabled'] && $config['collage']['only'])): ?>
+					<a href="#" class="<?php echo $btnClass; ?> takePic rotaryfocus"><i class="fa fa-camera"></i> <span data-i18n="takePhoto"></span></a>
+					<?php endif; ?>
+
 					<?php if ($config['collage']['enabled']): ?>
 					<a href="#" class="<?php echo $btnClass; ?> takeCollage rotaryfocus"><i class="fa fa-th-large"></i> <span
 							data-i18n="takeCollage"></span></a>
 					<?php endif; ?>
 
-					<?php if (!($config['collage']['enabled'] && $config['collage']['only'])): ?>
-					<a href="#" class="<?php echo $btnClass; ?> takePic rotaryfocus"><i class="fa fa-camera"></i> <span data-i18n="takePhoto"></span></a>
-					<?php endif; ?>
 				<?php endif; ?>
 				<button hidden class="closeGallery"></button>
 				<button hidden class="triggerPic"></button>
