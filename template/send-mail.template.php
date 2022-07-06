f75631952f6db544c0a73b705505f2137f5379ff
Andreas Blaesius <andi@unlegacy-android.org>
2022-07-06 20:55:26 +0200

    email: fix wrong button text if "Store email addresses in file" is enabled
    Change-Id: If23020a7bb476172d532a214a1f7da1939e54caf

--- template/send-mail.template.php
+++ template/send-mail.template.php
@@ -1,6 +1,10 @@
 	<div class="send-mail">
 		<i class="fa fa-times" id="send-mail-close"></i>
-		<p data-i18n="insertMail"></p>
+		<?php if ($config['mail']['send_all_later']): ?>
+		    <p data-i18n="insertMailToDB"></p>
+		<?php else: ?>
+		    <p data-i18n="insertMail"></p>
+		<?php endif; ?>
 		<form id="send-mail-form" style="margin: 0;">
 			<input class="mail-form-input" size="35" type="email" name="sendTo">
 			<input id="mail-form-image" type="hidden" name="image" value="">
@@ -8,9 +12,11 @@
 			<?php if ($config['mail']['send_all_later']): ?>
 				<input type="checkbox" id="mail-form-send-link" name="send-link" value="yes">
 				<label data-i18n="sendAllMail" for="mail-form-send-link"></label>
+				<button class="mail-form-input btn rotaryfocus" name="submit" type="submit" value="Send"><span data-i18n="add"></span></button>
+			<?php else: ?>
+				<button class="mail-form-input btn rotaryfocus" name="submit" type="submit" value="Send"><span data-i18n="send"></span></button>
 			<?php endif; ?>
 
-			<button class="mail-form-input btn rotaryfocus" name="submit" type="submit" value="Send"><span data-i18n="send"></span></button>
 		</form>
 
 		<div id="mail-form-message" style="max-width: 75%"></div>
