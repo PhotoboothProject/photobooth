b68174dec54a310e1ffe112dca414ee889ed23ee
Francesco <francesco.miccolis98@libero.it>
2022-10-22 11:01:01 +0200

    applyFrame: position the frame according to the preview

--- lib/applyFrame.php
+++ lib/applyFrame.php
@@ -1,8 +1,24 @@
 <?php
 
 function applyFrame($sourceResource, $framePath) {
+    $pic_width = imagesx($sourceResource);
+    $pic_height = imagesy($sourceResource);
+
     $frame = imagecreatefrompng($framePath);
-    $frame = resizePngImage($frame, imagesx($sourceResource), imagesy($sourceResource));
-    imagecopy($sourceResource, $frame, 0, 0, 0, 0, imagesx($frame), imagesy($frame));
+    $frame = resizePngImage($frame, $pic_width, $pic_height);
+
+    $frame_width = imagesx($frame);
+    $frame_height = imagesy($frame);
+
+    $src_x = 0;
+    $src_y = 0;
+
+    if($pic_height == $frame_height) {
+        $src_x = ($pic_width - $frame_width) / 2;
+    } else {
+        $src_y = ($pic_height - $frame_height) / 2;
+    }
+
+    imagecopy($sourceResource, $frame, 0, 0, $src_x, $src_y, $frame_width, $frame_height);
     return $sourceResource;
 }
