e7b7ede5ce49f725f2f3d1e144516af86b34c5be
Francesco <francesco.miccolis98@libero.it>
2022-10-22 11:08:38 +0200

    applyFrame: change src x and y in dst x and y

--- lib/applyFrame.php
+++ lib/applyFrame.php
@@ -10,15 +10,15 @@ function applyFrame($sourceResource, $framePath) {
     $frame_width = imagesx($frame);
     $frame_height = imagesy($frame);
 
-    $src_x = 0;
-    $src_y = 0;
+    $dst_x = 0;
+    $dst_y = 0;
 
     if($pic_height == $frame_height) {
-        $src_x = ($pic_width - $frame_width) / 2;
+        $dst_x = ($pic_width - $frame_width) / 2;
     } else {
-        $src_y = ($pic_height - $frame_height) / 2;
+        $dst_y = ($pic_height - $frame_height) / 2;
     }
 
-    imagecopy($sourceResource, $frame, 0, 0, $src_x, $src_y, $frame_width, $frame_height);
+    imagecopy($sourceResource, $frame, $dst_x, $dst_y, 0, 0, $frame_width, $frame_height);
     return $sourceResource;
 }
