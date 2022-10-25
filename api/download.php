be4e95fa54edc22a9312e2af341129ecd715a1af
Uwe Pieper <upiepr@gmail.com>
2022-10-25 11:24:01 +0200

    wip video qr

--- api/download.php
+++ api/download.php
@@ -1,19 +1,20 @@
 <?php
 require_once '../lib/config.php';
 
-$download_thumbs = $config['download']['thumbs'];
 $image = (isset($_GET['image']) && $_GET['image']) != '' ? $_GET['image'] : false;
 if ($image) {
     $fullres = $config['foldersRoot']['images'] . DIRECTORY_SEPARATOR . $image;
     $thumbres = $config['foldersRoot']['thumbs'] . DIRECTORY_SEPARATOR . $image;
 
-    if ($download_thumbs) {
+    $pathInfo = pathinfo($fullres);
+    if ($pathInfo['extension'] !== 'mp4' && $config['download']['thumbs']) {
         $filename_source = $thumbres;
     } else {
         $filename_source = $fullres;
     }
 
     header('Content-Type: application/octet-stream');
+    header('Content-Length: ' . filesize($fullres));
     header('Content-Disposition: attachment; filename="photobooth-' . $image . '"');
     echo file_get_contents(__DIR__ . '/../' . $filename_source);
     exit();
