4a855b403b388d303598aa814a4bba01a8b69158
Andreas Blaesius <andi@unlegacy-android.org>
2022-11-10 12:45:02 +0100

    restructure: create Photobooth and Helper classes
    Change-Id: I0be54d8e7e52c0eceee2f829992725405a338d9f

--- lib/helper.php
+++ lib/helper.php
@@ -1,87 +1,73 @@
 <?php
 require_once __DIR__ . '/log.php';
 
-function getPhotoboothPath() {
-    if (SERVER_OS == 'linux') {
-        $server_path = $_SERVER['DOCUMENT_ROOT'];
-    } else {
-        $server_path = str_replace('/', '\\', $_SERVER['DOCUMENT_ROOT']);
+class Photobooth {
+    public $server_ip;
+    public $os;
+    public $webRoot;
+    public $photoboothRoot;
+    public $isSubfolderInstall;
+    public $version;
+
+    function __construct() {
+        $this->server_ip = $this->get_ip();
+        $this->os = $this->server_os();
+        $this->webRoot = $this->get_web_root();
+        $this->photoboothRoot = Helper::get_rootpath();
+        $this->isSubfolderInstall = $this->detect_subfolder_install();
+        $this->version = $this->get_photobooth_version();
     }
 
-    return $server_path;
-}
-
-function isSubfolderInstall() {
-    $path = getrootpath(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR);
-    $server_path = getPhotoboothPath();
-
-    if ($path == $server_path) {
-        return false;
-    } else {
-        return true;
+    public static function server_os() {
+        return (DIRECTORY_SEPARATOR == '\\' || strtolower(substr(PHP_OS, 0, 3)) === 'win') ? 'windows' : 'linux';
     }
-}
 
-function getrootpath($relative_path) {
-    $realpath = realpath($relative_path);
-    $server_path = getPhotoboothPath();
-    $rootpath = str_replace($server_path, '', $realpath);
+    public static function get_ip() {
+        return self::server_os() == 'linux' ? shell_exec('hostname -I | cut -d " " -f 1') : $_SERVER['HTTP_HOST'];
+    }
 
-    return $rootpath;
-}
+    public static function get_web_root() {
+        return self::server_os() == 'linux' ? $_SERVER['DOCUMENT_ROOT'] : str_replace('/', '\\', $_SERVER['DOCUMENT_ROOT']);
+    }
 
-function fixSeperator($fix_path) {
-    $fixedPath = str_replace('\\', '/', $fix_path);
-    return $fixedPath;
-}
-
-function getPhotoboothIp() {
-    if (SERVER_OS == 'linux') {
-        $get_ip = shell_exec('hostname -I | cut -d " " -f 1');
-
-        if (!$get_ip) {
-            $ip = $_SERVER['HTTP_HOST'];
+    public static function get_photobooth_version() {
+        $packageJson = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'package.json';
+        if (is_file($packageJson)) {
+            $packageContent = file_get_contents($packageJson);
+            $package = json_decode($packageContent, true);
+            return $package['version'];
         } else {
-            $ip = $get_ip;
+            return 'unknown';
         }
-    } else {
-        $ip = $_SERVER['HTTP_HOST'];
     }
 
-    return $ip;
+    public static function detect_subfolder_install() {
+        return empty(Helper::get_rootpath()) ? false : true;
+    }
+
+    public static function get_url() {
+        if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') {
+            $protocol = 'https';
+        } else {
+            $protocol = 'http';
+        }
+        if (self::detect_subfolder_install()) {
+            $url = $protocol . '://' . self::get_ip() . Helper::get_rootpath();
+        } else {
+            $url = $protocol . '://' . self::get_ip();
+        }
+        return Helper::fix_seperator($url);
+    }
 }
 
-function getPhotoboothFolder() {
-    $path = getrootpath(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR);
-    $server_path = getPhotoboothPath();
-
-    if ($path == $server_path) {
-        return false;
-    } else {
-        $folder = str_replace($server_path, '', $path);
-        return $folder;
-    }
-}
-
-function getPhotoboothUrl() {
-    if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') {
-        $protocol = 'https';
-    } else {
-        $protocol = 'http';
+class Helper {
+    public static function get_rootpath($relative_path = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR) {
+        return str_replace($_SERVER['DOCUMENT_ROOT'], '', realpath($relative_path));
     }
 
-    $server_path = getPhotoboothPath();
-    $ip = getPhotoboothIp();
-    $path = getrootpath(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR);
-    if ($path == $server_path) {
-        $url = $protocol . '://' . $ip;
-    } else {
-        $folder = str_replace($server_path, '', $path);
-        $url = $protocol . '://' . $ip . $folder;
+    public static function fix_seperator($fix_path) {
+        return str_replace('\\', '/', $fix_path);
     }
-
-    $url = str_replace('\\', '/', $url);
-    return $url;
 }
 
 function testFile($file) {
@@ -101,10 +87,4 @@ function testFile($file) {
         return false;
     }
     return true;
-}
-
-function getPhotoboothVersion() {
-    $packageContent = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'package.json');
-    $package = json_decode($packageContent, true);
-    return $package['version'];
 }
