ec8853ff8b73f00edef827a2c50b665cfd0383ff
Andreas Blaesius <andi@unlegacy-android.org>
2022-11-12 12:49:27 +0100

    cleanup: remove experimental updater & dependencies checker
    This commit reverts https://github.com/andi34/photobooth/pull/285
    
    Change-Id: I720cb7f7d0ef30d379254f9e254253fed3254c9b

--- api/checkOS.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Content-Type: application/json');
-
-$operating_system = DIRECTORY_SEPARATOR == '\\' || strtolower(substr(PHP_OS, 0, 3)) === 'win' ? 'windows' : 'linux';
-
-echo json_encode([
-    'os' => $operating_system,
-]);
