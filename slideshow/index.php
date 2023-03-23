5d53bf6c8e5336f03c5aeb0791533d2c1a8893ea
Andreas Blaesius <andi@unlegacy-android.org>
2023-03-23 12:05:30 +0100

    slideshow: switch to PhotoSwipe v5
    Change-Id: I73fe22047f0a7ac03a6fb7ae130907eece7ef7ac

--- slideshow/index.php
+++ slideshow/index.php
@@ -2,16 +2,24 @@
 
 require_once('../lib/config.php');
 require_once('../lib/db.php');
-require_once('../lib/filter.php');
 
 if ($config['database']['enabled']) {
 	$images = getImagesFromDB();
 } else {
 	$images = getImagesFromDirectory($config['foldersAbs']['images']);
 }
+
 $imagelist = !empty($images) ? array_reverse($images) : $images;
 
+if (!empty($imagelist) && $config['slideshow']['randomPicture']) {
+    shuffle($imagelist);
+}
+
+$btnShape = 'shape--' . $config['ui']['button'];
 $uiShape = 'shape--' . $config['ui']['style'];
+$btnClass = 'btn btn--' . $config['ui']['button'];
+
+$GALLERY_FOOTER = false;
 ?>
 <!DOCTYPE html>
 <html>
@@ -22,7 +30,6 @@ $uiShape = 'shape--' . $config['ui']['style'];
 	<meta name="msapplication-TileColor" content="<?=$config['colors']['primary']?>">
 	<meta name="theme-color" content="<?=$config['colors']['primary']?>">
 	<meta http-equiv="refresh" content= "<?=$config['slideshow']['refreshTime']?>">
-
 
 	<title><?=$config['ui']['branding']?> Slideshow</title>
 
@@ -41,68 +48,77 @@ $uiShape = 'shape--' . $config['ui']['style'];
 	<link rel="stylesheet" href="../node_modules/font-awesome/css/font-awesome.css" />
 	<link rel="stylesheet" href="../node_modules/material-icons/iconfont/material-icons.css">
 	<link rel="stylesheet" href="../node_modules/material-icons/css/material-icons.css">
-	<link rel="stylesheet" href="../resources/css/slideshow.css?v=<?php echo $config['photobooth']['version']; ?>" />
+	<link rel="stylesheet" href="../vendor/PhotoSwipe/dist/photoswipe.css" />
+	<link rel="stylesheet" href="../resources/css/<?php echo $config['ui']['style']; ?>_style.css?v=<?php echo $config['photobooth']['version']; ?>" />
 	<?php if (is_file("../private/overrides.css")): ?>
 	<link rel="stylesheet" href="../private/overrides.css?v=<?php echo $config['photobooth']['version']; ?>" />
 	<?php endif; ?>
 </head>
 
-<body class="sshow">
-	<div class="images" id="slideshow">
-		<?php if (empty($imagelist)): ?>
-		<h1 data-i18n="gallery_no_image"></h1>
-		<?php else: ?>
-		<?php
-		if ($config['slideshow']['randomPicture']):
-		shuffle($imagelist);
-		endif;
-		?>
-		<?php foreach ($imagelist as $image): ?>
-		<?php
+<body class="deselect">
+	<div id="wrapper">
 
-		$date = $config['ui']['branding'] . ' Slideshow';
-		if ($config['picture']['naming'] === 'dateformatted' && $config['gallery']['show_date']) {
-			if ($config['database']['file'] != 'db') {
-				$db = strlen($config['database']['file']);
-				$name = substr($image, ++$db);
-			} else {
-				$name = $image;
-			}
-			$dateObject = DateTime::createFromFormat('Ymd_His', substr($name, 0, strlen($name) - 4));
-			if ($dateObject) {
-				$date = '<i class="' . $config['icons']['date'] . '"></i> ' . $dateObject->format($config['gallery']['date_format']);
-			}
-		}
-
-		if ($config['slideshow']['use_thumbs']) {
-			$filename_photo = '../' . $config['foldersRoot']['thumbs'] . DIRECTORY_SEPARATOR . $image;
-			if (!is_readable($filename_photo)) {
-				$filename_photo = '../' . $config['foldersRoot']['images'] . DIRECTORY_SEPARATOR . $image;
-			}
-		} else {
-			$filename_photo = '../' . $config['foldersRoot']['images'] . DIRECTORY_SEPARATOR . $image;
-		}
-
-		if (is_readable($filename_photo)) {
-		?>
-		<div class="center">
-			<figure>
-				<img class="<?php echo $uiShape; ?>" src="<?=$filename_photo?>" alt="<?=$image?>" />
-                <?php if ($config['gallery']['figcaption']): ?>
-                <figcaption class="<?php echo $uiShape; ?>"><?=$date?></figcaption>
-                <?php endif; ?>
-			</figure>
+<div id="gallery" class="gallery">
+	<div class="gallery__inner">
+		<div class="gallery__header">
+			<h1><span data-i18n="slideshow"></span></h1>
 		</div>
-		<?php } ?>
-		<?php endforeach; ?>
-		<?php endif; ?>
+		<div class="gallery__body" id="galimages">
+			<?php if (empty($imagelist)): ?>
+			<h1 style="text-align:center" data-i18n="gallery_no_image"></h1>
+			<?php else: ?>
+			<?php foreach ($imagelist as $image): ?>
+			<?php
+            $date = $config['ui']['branding'] . ' Slideshow';
+            if ($config['picture']['naming'] === 'dateformatted' && $config['gallery']['show_date']) {
+                if ($config['database']['file'] != 'db') {
+                    $db = strlen($config['database']['file']);
+                    $name = substr($image, ++$db);
+                } else {
+                    $name = $image;
+                }
+                $dateObject = DateTime::createFromFormat('Ymd_His', substr($name, 0, strlen($name) - 4));
+                if ($dateObject) {
+                    $date = '<i class="' . $config['icons']['date'] . '"></i> ' . $dateObject->format($config['gallery']['date_format']);
+                }
+            }
+
+            $filename_photo = '../' . $config['foldersRoot']['images'] . DIRECTORY_SEPARATOR . $image;
+            if (is_readable($filename_photo)) {
+                $filename_thumb = '../' . $config['foldersRoot']['thumbs'] . DIRECTORY_SEPARATOR . $image;
+
+                if (!is_readable($filename_thumb)) {
+                    $filename_thumb = '../' . $config['foldersRoot']['images'] . DIRECTORY_SEPARATOR . $image;
+                }
+                $imageinfo = getimagesize($filename_photo);
+                $imageinfoThumb = getimagesize($filename_thumb);
+            ?>
+
+			<a href="<?=$filename_photo?>" class="gallery__img rotaryfocus" data-size="<?=$imageinfo[0]?>x<?=$imageinfo[1]?>" data-pswp-width="<?=$imageinfo[0]?>" data-pswp-height="<?=$imageinfo[1]?>"
+				data-med="<?=$filename_thumb?>" data-med-size="<?=$imageinfoThumb[0]?>x<?=$imageinfoThumb[1]?>">
+				<figure class="<?php echo $uiShape; ?>">
+					<img class="<?php echo $uiShape; ?>" src="<?=$filename_thumb?>" alt="<?=$image?>" />
+                    <?php if ($config['gallery']['figcaption']): ?>
+                    <figcaption class="<?php echo $uiShape; ?>"><?=$date?></figcaption>
+                    <?php endif; ?>
+				</figure>
+			</a>
+	<?php } ?>
+				<?php endforeach; ?>
+				<?php endif; ?>
+		</div>
+	</div>
+</div>
 	</div>
 
 	<script src="../node_modules/whatwg-fetch/dist/fetch.umd.js"></script>
 	<script type="text/javascript" src="../api/config.php?v=<?php echo $config['photobooth']['version']; ?>"></script>
 	<script type="text/javascript" src="../node_modules/jquery/dist/jquery.min.js"></script>
-	<script type="text/javascript" src="../resources/js/theme.js?v=<?php echo $config['photobooth']['version']; ?>"></script>
+	<script type="text/javascript" src="../vendor/PhotoSwipe/dist/umd/photoswipe.umd.min.js"></script>
+	<script type="text/javascript" src="../vendor/PhotoSwipe/dist/umd/photoswipe-lightbox.umd.min.js"></script>
+	<script type="text/javascript" src="../resources/js/tools.js?v=<?php echo $config['photobooth']['version']; ?>"></script>
 	<script type="text/javascript" src="../resources/js/slideshow.js?v=<?php echo $config['photobooth']['version']; ?>"></script>
+	<script type="text/javascript" src="../resources/js/theme.js?v=<?php echo $config['photobooth']['version']; ?>"></script>
 	<script src="../node_modules/@andreasremdt/simple-translator/dist/umd/translator.min.js"></script>
 	<script type="text/javascript" src="../resources/js/i18n.js?v=<?php echo $config['photobooth']['version']; ?>"></script>
 </body>
