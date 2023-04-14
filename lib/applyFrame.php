dfda7ae4c06766dd633906406d5c9b15ef0fb4c9
Kevin St <bk.424b@gmail.com>
2023-04-14 17:57:41 +0200

    feature: extend the picture by the frame
    Change-Id: Ibf8663d3cdedadb02961b4b26530290e762af65c

--- lib/applyFrame.php
+++ lib/applyFrame.php
@@ -1,8 +1,37 @@
 <?php
+require_once __DIR__ . '/config.php';
 
 function applyFrame($sourceResource, $framePath) {
-    $pic_width = imagesx($sourceResource);
-    $pic_height = imagesy($sourceResource);
+    global $config;
+    if ($config['picture']['extend_by_frame']) {
+        $frame_left_percentage = $config['picture']['frame_left_percentage'];
+        $frame_right_percentage = $config['picture']['frame_right_percentage'];
+        $frame_top_percentage = $config['picture']['frame_top_percentage'];
+        $frame_bottom_percentage = $config['picture']['frame_bottom_percentage'];
+
+        $new_width = imagesx($sourceResource) / (1 - 0.01 * ($frame_left_percentage + $frame_right_percentage));
+        $new_height = imagesy($sourceResource) / (1 - 0.01 * ($frame_top_percentage + $frame_bottom_percentage));
+
+        $img = imagecreatetruecolor($new_width, $new_height);
+        $white = imagecolorallocate($img, 255, 255, 255);
+
+        // We fill in the new white image
+        imagefill($img, 0, 0, $white);
+
+        $image_pos_x = imagesx($img) * 0.01 * $frame_left_percentage;
+        $image_pos_y = imagesy($img) * 0.01 * $frame_top_percentage;
+
+        // We copy the image to which we want to apply the frame in our new image.
+        imagecopy($img, $sourceResource, $image_pos_x, $image_pos_y, 0, 0, imagesx($sourceResource), imagesy($sourceResource));
+
+        // Clear cache
+        imagedestroy($sourceResource);
+    } else {
+        $img = $sourceResource;
+    }
+
+    $pic_width = imagesx($img);
+    $pic_height = imagesy($img);
 
     $frame = imagecreatefrompng($framePath);
     $frame = resizePngImage($frame, $pic_width, $pic_height);
@@ -19,6 +48,6 @@ function applyFrame($sourceResource, $framePath) {
         $dst_y = intval(($pic_height - $frame_height) / 2);
     }
 
-    imagecopy($sourceResource, $frame, $dst_x, $dst_y, 0, 0, $frame_width, $frame_height);
-    return $sourceResource;
+    imagecopy($img, $frame, $dst_x, $dst_y, 0, 0, $frame_width, $frame_height);
+    return $img;
 }
