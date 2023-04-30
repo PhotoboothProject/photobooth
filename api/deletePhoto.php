15bfd373c949d2f51109ce1e5050380b65df8941
Andreas Blaesius <andi@unlegacy-android.org>
2023-04-30 14:29:41 +0200

    lib: add FileDelete class for file removing
    Change-Id: Ic0cfd27a52a812a06359288491b3f7373baee510

--- api/deletePhoto.php
+++ api/deletePhoto.php
@@ -1,56 +1,35 @@
 <?php
+
 header('Content-Type: application/json');
 
 require_once '../lib/db.php';
 require_once '../lib/config.php';
 require_once '../lib/log.php';
+require_once '../lib/deleteFile.php';
 
 if (empty($_POST['file'])) {
     $errormsg = basename($_SERVER['PHP_SELF']) . ': No file provided';
     logErrorAndDie($errormsg);
 }
 
-$images = [];
-$unavailableImages = [];
-$failedImages = [];
 $file = $_POST['file'];
-$success = true;
-$images = [
-    $config['foldersAbs']['images'] . DIRECTORY_SEPARATOR . $file,
-    $config['foldersAbs']['thumbs'] . DIRECTORY_SEPARATOR . $file,
-    $config['foldersAbs']['keying'] . DIRECTORY_SEPARATOR . $file,
-];
+$paths = [$config['foldersAbs']['images'], $config['foldersAbs']['thumbs'], $config['foldersAbs']['keying']];
 
 if (!$config['picture']['keep_original']) {
-    $images[] = $config['foldersAbs']['tmp'] . DIRECTORY_SEPARATOR . $file;
+    $paths[] = $config['foldersAbs']['tmp'];
 }
-
-foreach ($images as $image) {
-    if (is_readable($image)) {
-        if (!unlink($image)) {
-            $errormsg = basename($_SERVER['PHP_SELF']) . ': Could not delete ' . $image;
-            logError($errormsg);
-            $success = false;
-            $failedImages[] = $image;
-        }
-    } else {
-        $unavailableImages[] = $image;
-    }
-}
+$delete = new FileDelete($file, $paths);
+$delete->deleteFiles();
+$logData = $delete->getLogData();
 
 if ($config['database']['enabled']) {
     $database = new DatabaseManager(DB_FILE, IMG_DIR);
     $database->deleteFileFromDB($file);
 }
 
-$LogData = [
-    'success' => $success,
-    'file' => $file,
-    'unavailable' => $unavailableImages,
-    'failed' => $failedImages,
-];
-$LogString = json_encode($LogData);
-if (!$success || $config['dev']['loglevel'] > 1) {
-    logError($LogData);
+$logString = json_encode($logData);
+if (!$logData['success'] || $config['dev']['loglevel'] > 1) {
+    logError($logData);
 }
-echo $LogString;
+
+echo $logString;
---
+++ lib/deleteFile.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * Class FileDelete.
+ */
+class FileDelete {
+    /** @var string The filename of the file. */
+    private $file;
+
+    /** @var array The paths of the files to be deleted. */
+    private $paths = [];
+
+    /** @var array The file paths of the files that could not be deleted. */
+    private $unavailableFiles = [];
+
+    /** @var array The file paths of the files that were attempted to be deleted, but failed. */
+    private $failedFiles = [];
+
+    /** @var bool Whether or not the deletion of the filess was successful. */
+    private $success = true;
+
+    /**
+     * FileDelete constructor.
+     *
+     * @param string $file The filename of the file.
+     * @param array $paths The file paths of the files.
+     */
+    public function __construct($file, $paths) {
+        $this->file = $file;
+        $this->paths = $paths;
+    }
+
+    /**
+     * Deletes the files.
+     */
+    public function deleteFiles() {
+        foreach ($this->paths as $file) {
+            $file = $file . DIRECTORY_SEPARATOR . $this->file;
+            if (is_readable($file)) {
+                if (!unlink($file)) {
+                    $this->success = false;
+                    $this->failedFiles[] = $file;
+                }
+            } else {
+                $this->unavailableFiles[] = $file;
+            }
+        }
+    }
+
+    /**
+     * Sets the paths.
+     *
+     * @param array $paths The file paths of the files.
+     */
+    public function setPaths(array $paths) {
+        $this->paths = $paths;
+    }
+
+    /**
+     * Sets the unavailable files.
+     *
+     * @param array $unavailableFiles The file paths of the unavailable files.
+     */
+    public function setUnavailableFiles(array $unavailableFiles) {
+        $this->unavailableFiles = $unavailableFiles;
+    }
+
+    /**
+     * Sets the failed files.
+     *
+     * @param array $failedFiles The file incl. paths of the failed files.
+     */
+    public function setFailedFiles(array $failedFiles) {
+        $this->failedFiles = $failedFiles;
+    }
+
+    /**
+     * Gets the log data.
+     *
+     * @return array The log data.
+     */
+    public function getLogData() {
+        return [
+            'success' => $this->success,
+            'file' => $this->file,
+            'unavailable' => $this->unavailableFiles,
+            'failed' => $this->failedFiles,
+        ];
+    }
+}
