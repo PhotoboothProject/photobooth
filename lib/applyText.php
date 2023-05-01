91ff36e09eef2cde8391f5a01c20b128c4f9c41f
Andreas Blaesius <andi@unlegacy-android.org>
2023-05-01 20:18:00 +0200

    lib(applyText): add error handling, return unmodified resource on error
    Change-Id: I062a0a66b1ededa30d08bfe91e47fab7c53d58bf

--- lib/applyText.php
+++ lib/applyText.php
@@ -1,21 +1,36 @@
 <?php
 
 function applyText($sourceResource, $fontSize, $fontRotation, $fontLocationX, $fontLocationY, $fontColor, $fontPath, $line1Text, $line2Text, $line3Text, $lineSpacing) {
-    list($r, $g, $b) = sscanf($fontColor, '#%02x%02x%02x');
-    $color = imagecolorallocate($sourceResource, $r, $g, $b);
+    try {
+        // Convert hex color string to RGB values
+        list($r, $g, $b) = sscanf($fontColor, '#%02x%02x%02x');
 
-    if (!empty($line1Text)) {
-        imagettftext($sourceResource, $fontSize, $fontRotation, $fontLocationX, $fontLocationY, $color, $fontPath, $line1Text);
+        // Allocate color and set font
+        $color = imagecolorallocate($sourceResource, $r, $g, $b);
+
+        // Add first line of text
+        if (!empty($line1Text)) {
+            imagettftext($sourceResource, $fontSize, $fontRotation, $fontLocationX, $fontLocationY, $color, $fontPath, $line1Text);
+        }
+
+        // Add second line of text
+        if (!empty($line2Text)) {
+            $line2Y = $fontRotation < 45 && $fontRotation > -45 ? $fontLocationY + $lineSpacing : $fontLocationY;
+            $line2X = $fontRotation < 45 && $fontRotation > -45 ? $fontLocationX : $fontLocationX + $lineSpacing;
+            imagettftext($sourceResource, $fontSize, $fontRotation, $line2X, $line2Y, $color, $fontPath, $line2Text);
+        }
+
+        // Add third line of text
+        if (!empty($line3Text)) {
+            $line3Y = $fontRotation < 45 && $fontRotation > -45 ? $fontLocationY + $lineSpacing * 2 : $fontLocationY;
+            $line3X = $fontRotation < 45 && $fontRotation > -45 ? $fontLocationX : $fontLocationX + $lineSpacing * 2;
+            imagettftext($sourceResource, $fontSize, $fontRotation, $line3X, $line3Y, $color, $fontPath, $line3Text);
+        }
+    } catch (Exception $e) {
+        // Return unmodified resource
+        return $sourceResource;
     }
-    if (!empty($line2Text)) {
-        $line2Y = $fontRotation < 45 && $fontRotation > -45 ? $fontLocationY + $lineSpacing : $fontLocationY;
-        $line2X = $fontRotation < 45 && $fontRotation > -45 ? $fontLocationX : $fontLocationX + $lineSpacing;
-        imagettftext($sourceResource, $fontSize, $fontRotation, $line2X, $line2Y, $color, $fontPath, $line2Text);
-    }
-    if (!empty($line3Text)) {
-        $line3Y = $fontRotation < 45 && $fontRotation > -45 ? $fontLocationY + $lineSpacing * 2 : $fontLocationY;
-        $line3X = $fontRotation < 45 && $fontRotation > -45 ? $fontLocationX : $fontLocationX + $lineSpacing * 2;
-        imagettftext($sourceResource, $fontSize, $fontRotation, $line3X, $line3Y, $color, $fontPath, $line3Text);
-    }
+
+    // Return resource with text applied
     return $sourceResource;
 }
