e6489857e3330500dfebc2d38caf043b760bd7f6
Andreas Blaesius <andi@unlegacy-android.org>
2023-05-01 20:22:32 +0200

    lib(applyFrame): add error handling, return unmodified resource on error
    Change-Id: I69b80cb9203263d8eb3b7783009f8dd038da00e5

--- lib/applyFrame.php
+++ lib/applyFrame.php
@@ -1,24 +1,36 @@
 <?php
 
 function applyFrame($sourceResource, $framePath) {
-    $pic_width = imagesx($sourceResource);
-    $pic_height = imagesy($sourceResource);
+    try {
+        $img = $sourceResource;
+        $pic_width = imagesx($img);
+        $pic_height = imagesy($img);
 
-    $frame = imagecreatefrompng($framePath);
-    $frame = resizePngImage($frame, $pic_width, $pic_height);
+        $frame = imagecreatefrompng($framePath);
+        $frame = resizePngImage($frame, $pic_width, $pic_height);
 
-    $frame_width = imagesx($frame);
-    $frame_height = imagesy($frame);
+        $frame_width = imagesx($frame);
+        $frame_height = imagesy($frame);
 
-    $dst_x = 0;
-    $dst_y = 0;
+        $dst_x = 0;
+        $dst_y = 0;
 
-    if ($pic_height == $frame_height) {
-        $dst_x = intval(($pic_width - $frame_width) / 2);
-    } else {
-        $dst_y = intval(($pic_height - $frame_height) / 2);
+        if ($pic_height == $frame_height) {
+            $dst_x = intval(($pic_width - $frame_width) / 2);
+        } else {
+            $dst_y = intval(($pic_height - $frame_height) / 2);
+        }
+
+        if (!imagecopy($img, $frame, $dst_x, $dst_y, 0, 0, $frame_width, $frame_height)) {
+            throw new Exception('Error applying frame to image.');
+        }
+    } catch (Exception $e) {
+        // Clear cache
+        imagedestroy($img);
+        // Return unmodified resource
+        return $sourceResource;
     }
 
-    imagecopy($sourceResource, $frame, $dst_x, $dst_y, 0, 0, $frame_width, $frame_height);
-    return $sourceResource;
+    // Return resource with text applied
+    return $img;
 }
