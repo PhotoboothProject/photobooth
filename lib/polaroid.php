a869f1b866fb30d72a7257f791e99ee204091b99
Andreas Blaesius <andi@unlegacy-android.org>
2023-05-02 08:15:00 +0200

    lib(polaroid): throw exceptions if needed
    Change-Id: I72d2fe931e47d68132efe056ee60e48276b614b8

--- lib/polaroid.php
+++ lib/polaroid.php
@@ -14,13 +14,21 @@ function effectPolaroid($resource, $rotation, $rbcc, $gbcc, $bbcc) {
     try {
         // We create a new image
         $img = imagecreatetruecolor(imagesx($resource) + 25, imagesy($resource) + 80);
+        if (!$img) {
+            throw new Exception('Cannot create new image.');
+        }
         $white = imagecolorallocate($img, 255, 255, 255);
 
         // We fill in the new white image
-        imagefill($img, 0, 0, $white);
+        if (!imagefill($img, 0, 0, $white)) {
+            throw new Exception('Cannot fill image.');
+        }
 
         // We copy the image to which we want to apply the polariod effect in our new image.
-        imagecopy($img, $resource, 11, 11, 0, 0, imagesx($resource), imagesy($resource));
+        if (!imagecopy($img, $resource, 11, 11, 0, 0, imagesx($resource), imagesy($resource))) {
+            imagedestroy($img);
+            throw new Exception('Cannot copy image.');
+        }
 
         // Clear cach
         imagedestroy($resource);
@@ -28,7 +36,10 @@ function effectPolaroid($resource, $rotation, $rbcc, $gbcc, $bbcc) {
         // Border color
         $color = imagecolorallocate($img, 192, 192, 192);
         // We put a gray border to our image.
-        imagerectangle($img, 0, 0, imagesx($img) - 4, imagesy($img) - 4, $color);
+        if (!imagerectangle($img, 0, 0, imagesx($img) - 4, imagesy($img) - 4, $color)) {
+            imagedestroy($img);
+            throw new Exception('Cannot add border.');
+        }
 
         // Shade Colors
         $gris1 = imagecolorallocate($img, 208, 208, 208);
@@ -36,16 +47,25 @@ function effectPolaroid($resource, $rotation, $rbcc, $gbcc, $bbcc) {
         $gris3 = imagecolorallocate($img, 240, 240, 240);
 
         // We add a small shadow
-        imageline($img, 2, imagesy($img) - 3, imagesx($img) - 1, imagesy($img) - 3, $gris1);
-        imageline($img, 4, imagesy($img) - 2, imagesx($img) - 1, imagesy($img) - 2, $gris2);
-        imageline($img, 6, imagesy($img) - 1, imagesx($img) - 1, imagesy($img) - 1, $gris3);
-        imageline($img, imagesx($img) - 3, 2, imagesx($img) - 3, imagesy($img) - 4, $gris1);
-        imageline($img, imagesx($img) - 2, 4, imagesx($img) - 2, imagesy($img) - 4, $gris2);
-        imageline($img, imagesx($img) - 1, 6, imagesx($img) - 1, imagesy($img) - 4, $gris3);
+        if (
+            !imageline($img, 2, imagesy($img) - 3, imagesx($img) - 1, imagesy($img) - 3, $gris1) ||
+            !imageline($img, 4, imagesy($img) - 2, imagesx($img) - 1, imagesy($img) - 2, $gris2) ||
+            !imageline($img, 6, imagesy($img) - 1, imagesx($img) - 1, imagesy($img) - 1, $gris3) ||
+            !imageline($img, imagesx($img) - 3, 2, imagesx($img) - 3, imagesy($img) - 4, $gris1) ||
+            !imageline($img, imagesx($img) - 2, 4, imagesx($img) - 2, imagesy($img) - 4, $gris2) ||
+            !imageline($img, imagesx($img) - 1, 6, imagesx($img) - 1, imagesy($img) - 4, $gris3)
+        ) {
+            imagedestroy($img);
+            throw new Exception('Cannot add shadow.');
+        }
 
         // We rotate the image
         $background = imagecolorallocate($img, $rbcc, $gbcc, $bbcc);
         $rotatedImg = imagerotate($img, $rotation, $background);
+
+        if (!$rotatedImg) {
+            throw new Exception('Cannot rotate image.');
+        }
     } catch (Exception $e) {
         // Clear cache
         imagedestroy($img);
