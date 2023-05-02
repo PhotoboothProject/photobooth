bd6b4fc287b8b62600bb943e9d59763792e13e5b
Andreas Blaesius <andi@unlegacy-android.org>
2023-05-02 08:26:26 +0200

    lib(resize): add try-catch-block to resizeImage function
    Change-Id: I00afe3cd42fafe7974341acb9134ffe835ef48f1

--- lib/resize.php
+++ lib/resize.php
@@ -70,19 +70,24 @@ function rotateResizeImage($image, $rotation, $bg_color = '#ffffff') {
 }
 
 function resizeImage($image, $max_width, $max_height) {
-    if (!$image) {
-        return false;
+    try {
+        if (!$image) {
+            throw new InvalidArgumentException('Invalid image resource');
+        }
+
+        $old_width = imagesx($image);
+        $old_height = imagesy($image);
+
+        $scale = min($max_width / $old_width, $max_height / $old_height);
+
+        $new_width = ceil($scale * $old_width);
+        $new_height = ceil($scale * $old_height);
+
+        return imagescale($image, $new_width, $new_height, IMG_TRIANGLE);
+    } catch (Exception $e) {
+        // Return unmodified resource
+        return $image;
     }
-
-    $old_width = imagesx($image);
-    $old_height = imagesy($image);
-
-    $scale = min($max_width / $old_width, $max_height / $old_height);
-
-    $new_width = ceil($scale * $old_width);
-    $new_height = ceil($scale * $old_height);
-
-    return imagescale($image, $new_width, $new_height, IMG_TRIANGLE);
 }
 
 function resizePngImage($image, $max_width, $max_height) {
