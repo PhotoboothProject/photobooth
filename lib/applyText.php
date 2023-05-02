02bcd3c2e40585857d105f333ca29bd7e598a428
Andreas Blaesius <andi@unlegacy-android.org>
2023-05-02 08:05:43 +0200

    lib(applyText): throw Exception if text can't be applied
    Change-Id: I4ed17534788292f672ccc0c8c342c287378ce729

--- lib/applyText.php
+++ lib/applyText.php
@@ -10,21 +10,27 @@ function applyText($sourceResource, $fontSize, $fontRotation, $fontLocationX, $f
 
         // Add first line of text
         if (!empty($line1Text)) {
-            imagettftext($sourceResource, $fontSize, $fontRotation, $fontLocationX, $fontLocationY, $color, $fontPath, $line1Text);
+            if (imagettftext($sourceResource, $fontSize, $fontRotation, $fontLocationX, $fontLocationY, $color, $fontPath, $line1Text)) {
+                throw new Exception('Could not add first line of text to resource.');
+            }
         }
 
         // Add second line of text
         if (!empty($line2Text)) {
             $line2Y = $fontRotation < 45 && $fontRotation > -45 ? $fontLocationY + $lineSpacing : $fontLocationY;
             $line2X = $fontRotation < 45 && $fontRotation > -45 ? $fontLocationX : $fontLocationX + $lineSpacing;
-            imagettftext($sourceResource, $fontSize, $fontRotation, $line2X, $line2Y, $color, $fontPath, $line2Text);
+            if (imagettftext($sourceResource, $fontSize, $fontRotation, $line2X, $line2Y, $color, $fontPath, $line2Text)) {
+                throw new Exception('Could not add second line of text to resource.');
+            }
         }
 
         // Add third line of text
         if (!empty($line3Text)) {
             $line3Y = $fontRotation < 45 && $fontRotation > -45 ? $fontLocationY + $lineSpacing * 2 : $fontLocationY;
             $line3X = $fontRotation < 45 && $fontRotation > -45 ? $fontLocationX : $fontLocationX + $lineSpacing * 2;
-            imagettftext($sourceResource, $fontSize, $fontRotation, $line3X, $line3Y, $color, $fontPath, $line3Text);
+            if (imagettftext($sourceResource, $fontSize, $fontRotation, $line3X, $line3Y, $color, $fontPath, $line3Text)) {
+                throw new Exception('Could not add third line of text to resource.');
+            }
         }
     } catch (Exception $e) {
         // Return unmodified resource
