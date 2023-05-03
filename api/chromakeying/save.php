1e49b657addc9cc5798aac6892fa1709d10bc3ad
Andreas Blaesius <andi@unlegacy-android.org>
2023-05-03 07:26:21 +0200

    api (chromakeying): add error handling
    Change-Id: Ie66947d965160fb3fd96668580908b5770d5c988

--- api/chromakeying/save.php
+++ api/chromakeying/save.php
@@ -1,6 +1,16 @@
 <?php
 header('Content-Type: application/json');
 
+if (!isset($_POST['imgData']) || empty($_POST['imgData'])) {
+    http_response_code(400);
+    $logData = [
+        'success' => false,
+        'error' => 'imgData not set or empty.',
+    ];
+    $logString = json_encode($logData);
+    die($logString);
+}
+
 require_once '../../lib/config.php';
 require_once '../../lib/db.php';
 require_once '../../lib/image.php';
@@ -19,31 +29,68 @@ $filename_keying = $config['foldersAbs']['keying'] . DIRECTORY_SEPARATOR . $file
 $picture_permissions = $config['picture']['permissions'];
 $thumb_size = substr($config['picture']['thumb_size'], 0, -2);
 
-$img = $_POST['imgData'];
-$img = str_replace('data:image/png;base64,', '', $img);
-$img = str_replace(' ', '+', $img);
-$data = base64_decode($img);
-$imageResource = imagecreatefromstring($data);
-imagejpeg($imageResource, $filename_photo, $config['jpeg_quality']['image']);
-copy($filename_photo, $filename_keying);
+try {
+    $img = $_POST['imgData'];
+    $img = str_replace('data:image/png;base64,', '', $img);
+    $img = str_replace(' ', '+', $img);
+    $data = base64_decode($img);
+    $imageResource = imagecreatefromstring($data);
+    if (!$imageResource) {
+        throw new Exception('Failed to create image from data.');
+    }
+    if (!imagejpeg($imageResource, $filename_photo, $config['jpeg_quality']['image'])) {
+        throw new Exception('Failed to save ' . $filename_photo);
+    }
+    if (!copy($filename_photo, $filename_keying)) {
+        throw new Exception('Failed to save ' . $filename_keying);
+    }
 
-// image scale, create thumbnail
-$thumbResource = resizeImage($imageResource, $thumb_size, $thumb_size);
-imagejpeg($thumbResource, $filename_thumb, $config['jpeg_quality']['thumb']);
-imagedestroy($thumbResource);
+    // image scale, create thumbnail
+    $thumbResource = resizeImage($imageResource, $thumb_size, $thumb_size);
+    if (!$thumbResource) {
+        throw new Exception('Failed to resize thumbnail.');
+    }
+    if (!imagejpeg($thumbResource, $filename_thumb, $config['jpeg_quality']['thumb'])) {
+        throw new Exception('Failed to save thumbnail.');
+    }
 
-imagedestroy($imageResource);
+    // clear cache
+    imagedestroy($thumbResource);
+    imagedestroy($imageResource);
 
-// insert into database
-if ($config['database']['enabled']) {
-    $database->appendFileToDB($file);
+    // insert into database
+    if ($config['database']['enabled']) {
+        if (!$database->appendFileToDB($file)) {
+            throw new Exception('Failed to add ' . $file . ' to database.');
+        }
+    }
+
+    // Change permissions
+    if (!chmod($filename_photo, octdec($picture_permissions))) {
+        throw new Exception('Failed to change file permissions.');
+    }
+} catch (Exception $e) {
+    // Try to clear cache
+    if (is_resource($thumbResource)) {
+        imagedestroy($thumbResource);
+    }
+    if (is_resource($imageResource)) {
+        imagedestroy($imageResource);
+    }
+    $logData = [
+        'success' => false,
+        'error' => $e->getMessage(),
+        'php' => basename($_SERVER['PHP_SELF']),
+    ];
+    $logString = json_encode($logData);
+    die($logString);
 }
 
-// Change permissions
-chmod($filename_photo, octdec($picture_permissions));
-
 // send imagename to frontend
-echo json_encode([
+$logData = [
     'success' => true,
     'filename' => $file,
-]);
+    'php' => basename($_SERVER['PHP_SELF']),
+];
+$logString = json_encode($logData);
+die($logString);
