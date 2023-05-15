c3f0190be98c96f06427a1df7722ef19fce7b63e
Andreas Blaesius <andi@unlegacy-android.org>
2023-05-15 07:09:07 +0200

    lib (log): add DataLogger class

--- lib/log.php
+++ lib/log.php
@@ -1,6 +1,55 @@
 <?php
 require_once __DIR__ . '/config.php';
 
+class DataLogger {
+    public $logFile;
+    public $logData = [];
+
+    public function __construct($logFile) {
+        $this->logFile = $logFile;
+    }
+
+    public function addLogData($data) {
+        $this->logData[] = $data;
+    }
+
+    public function getLogData() {
+        return $this->logData;
+    }
+
+    public function logToFile($data = null) {
+        if ($data === null) {
+            $data = $this->getLogData();
+        }
+        if (!empty($data)) {
+            $logFile = $this->logFile;
+            $fileData = date('c') . ":\n" . print_r($data, true) . "\n";
+            if (is_file($logFile)) {
+                $fileData .= file_get_contents($logFile);
+            }
+            file_put_contents($logFile, $fileData);
+            $this->logData = [];
+        }
+    }
+
+    public function logErrorAndDie($errormsg) {
+        try {
+            $errorData = [
+                'error' => $errormsg,
+            ];
+            $errorString = json_encode($errorData);
+            self::logToFile($errorData);
+            die($errorString);
+        } catch (Exception $e) {
+            $errorData = [
+                'error' => $e->getMessage(),
+            ];
+            $errorString = json_encode($errorData);
+            die($errorString);
+        }
+    }
+}
+
 function logError($data) {
     global $config;
     $logfile = $config['foldersAbs']['tmp'] . DIRECTORY_SEPARATOR . $config['dev']['logfile'];
