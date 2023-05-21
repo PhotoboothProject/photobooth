36408fed8fd89e723d351a3d79b87430079e9ff6
cfmjk <jk@cfmmedia.de>
2023-05-21 10:00:52 +0200

    admin helper

---
+++ admin/helper/index.php
@@ -0,0 +1,8 @@
+<?php
+    include("loader.php");
+    include("hiddenElement.php");
+    include("indent.php");
+    include("ctaBtn.php");
+    include("menuBtn.php");
+    include("toast.php");
+?>
