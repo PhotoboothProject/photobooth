fe31750e159e0cd2219a2b9a95782abf55b2145d
cfmjk <jk@cfmmedia.de>
2023-05-21 10:02:30 +0200

    login new admin style

--- login/index.php
+++ login/index.php
@@ -19,8 +19,6 @@ if (isset($_POST['submit'])) {
 }
 // END LOGIN
 
-$btnClass = 'btn btn--login shape--' . $config['ui']['button'];
-$uiShape = 'shape--' . $config['ui']['style'];
 ?>
 <!DOCTYPE html>
 <html>
@@ -48,66 +46,116 @@ $uiShape = 'shape--' . $config['ui']['style'];
 	<link rel="stylesheet" href="../node_modules/font-awesome/css/font-awesome.css" />
 	<link rel="stylesheet" href="../node_modules/material-icons/iconfont/material-icons.css">
 	<link rel="stylesheet" href="../node_modules/material-icons/css/material-icons.css">
-	<link rel="stylesheet" href="../resources/css/login.css?v=<?php echo $config['photobooth']['version']; ?>" />
-	<?php if (is_file("../private/overrides.css")): ?>
-	<link rel="stylesheet" href="../private/overrides.css?v=<?php echo $config['photobooth']['version']; ?>" />
-	<?php endif; ?>
+
+	<!-- tw admin -->
+	<link rel="stylesheet" href="../resources/css/tailwind.admin.css"/>
 </head>
+<?php
+	include("../admin/helper/index.php");
 
-<body class="loginbody">
-	<div class="login-panel <?php echo $uiShape; ?>">
-		<h2><?=$config['ui']['branding']?> Login</h2>
-		<hr>
+	$labelClass = "w-full flex flex-col mb-1";
+	$inputClass = "w-full h-10 border border-solid border-gray-300 focus:border-brand-1 rounded-md px-3 mt-auto";
+	$btnClass = "w-full h-12 rounded-full bg-brand-1 text-white flex items-center justify-center relative ml-auto border-2 border-solid border-brand-1 hover:bg-white hover:text-brand-1 transition font-bold px-4";
+?>
+
+<body>
+	<div class="w-full h-screen grid place-items-center absolute bg-brand-1 px-6 py-12 overflow-x-hidden overflow-y-auto">
+		<div class="w-full flex items-center justify-center flex-col">
+
+		<!-- login -->
 		<?php if($config['login']['enabled'] && !(isset($_SESSION['auth']) && $_SESSION['auth'] === true)): ?>
-		<form method='post' class="login">
-			<label for="username"><span data-i18n="login_username"></span></label>
-			<input type="text" name="username" id="username" autocomplete="on" required>
-			<label for="password"><span data-i18n="login_password"></span></label>
-			</br>
-			<input type="password" name="password" id="password" autocomplete="on" required>
-			<span toggle="#password" class="password-toggle <?php echo $config['icons']['password_visibility']; ?>"></span>
-			<p><input type="submit" name="submit" value="Login" class="btn btn--tiny btn--flex <?php echo $uiShape; ?>"></p>
-			<?php if ($error !== false) {
-				echo '<p style="color: red;"><span data-i18n="login_invalid"></span></p>';
-			} ?>
-		</form>
-		<hr>
-		<?php endif; ?>
-		<?php if(!$config['protect']['admin'] || (!$config['protect']['localhost_admin'] && $_SERVER['REMOTE_ADDR'] === $_SERVER['SERVER_ADDR']) || !$config['login']['enabled'] || (isset($_SESSION['auth']) && $_SESSION['auth'] === true)): ?>
-		<p><a href="../admin" class="<?php echo $btnClass; ?>"><i class="<?php echo $config['icons']['admin']; ?>"></i> <span data-i18n="admin_panel"></span></a></p>
-		<?php endif; ?>
-		<p><a href="../gallery.php" class="<?php echo $btnClass; ?>"><i class="<?php echo $config['icons']['gallery']; ?>"></i> <span data-i18n="gallery"></span></a></p>
-		<p><a href="../slideshow" class="<?php echo $btnClass; ?>"><i class="<?php echo $config['icons']['slideshow']; ?>"></i> <span data-i18n="slideshow"></span></a></p>
-		<?php if(!$config['protect']['index'] || (!$config['protect']['localhost_index'] && $_SERVER['REMOTE_ADDR'] === $_SERVER['SERVER_ADDR']) || !$config['login']['enabled'] || (isset($_SESSION['auth']) && $_SESSION['auth'] === true)): ?>
-		<p><a href="../livechroma.php" class="<?php echo $btnClass; ?>"><i class="<?php echo $config['icons']['livechroma']; ?>"></i> <span data-i18n="livechroma"></span></a></p>
-		<?php endif; ?>
-		<?php if(!$config['protect']['manual'] || (!$config['protect']['localhost_manual'] && $_SERVER['REMOTE_ADDR'] === $_SERVER['SERVER_ADDR']) || !$config['login']['enabled'] || (isset($_SESSION['auth']) && $_SESSION['auth'] === true)): ?>
-		<p><a href="../faq" class="<?php echo $btnClass; ?>" title="FAQ" target="newwin"><i class="<?php echo $config['icons']['faq']; ?>"></i> <span data-i18n="show_faq"></span></a></p>
-		<p><a href="../manual" class="<?php echo $btnClass; ?>" title="Manual" target="newwin"><i class="<?php echo $config['icons']['manual']; ?>"></i> <span data-i18n="show_manual"></span></a></p>
-		<p><a href="https://t.me/PhotoboothGroup" class="<?php echo $btnClass; ?>" title="Telegram" target="newwin"><i class="<?php echo $config['icons']['telegram']; ?>"></i> <span data-i18n="telegram"></span></a></p>
-		<?php endif; ?>
-		<p><a href="./" class="<?php echo $btnClass; ?>"><i class="<?php echo $config['icons']['refresh']; ?>"></i> <span data-i18n="reload"></span></a></p>
-		<?php if(!$config['protect']['index'] || (!$config['protect']['localhost_index'] && $_SERVER['REMOTE_ADDR'] === $_SERVER['SERVER_ADDR']) || !$config['login']['enabled'] || (isset($_SESSION['auth']) && $_SESSION['auth'] === true)): ?>
-		<p><a href="../" class="<?php echo $btnClass; ?>" ><i class="<?php echo $config['icons']['close']; ?>"></i> <span data-i18n="close"></span></a></p>
-		<?php endif; ?>
-		<?php if(isset($_SESSION['auth']) && $_SESSION['auth'] === true): ?>
-		<p><a href="logout.php" class="<?php echo $btnClass; ?>"><i class="<?php echo $config['icons']['logout']; ?>"></i> <span data-i18n="logout"></span></a></p>
-		<?php endif; ?>
-	</div>
+			<div class="w-full max-w-xl h-144 rounded-lg p-8 bg-white flex flex-col shadow-xl">
+				<form method="post">
 
-	<div id="adminsettings">
-		<div style="position:absolute; bottom:0; right:0;">
-			<img src="../resources/img/spacer.png" alt="adminsettings" ondblclick="adminsettings()" />
+					<div class="w-full flex flex-col items-center justify-center text-2xl font-bold text-brand-1 mb-2">
+						<?=$config['ui']['branding']?> Login
+					</div>
+
+					<div class="w-full text-center text-gray-500 mb-8">
+						Bitte logge dich ein um fortzufahren.
+					</div>
+
+					<!-- user -->
+					<div class="relative">
+						<label class="<?=$labelClass?>" for="username"><span data-i18n="login_username"></span></label>
+						<input class="<?=$inputClass?>" type="text" name="username" id="username" autocomplete="on" required>
+					</div>
+
+					<!-- pw -->
+					<div class="relative mt-2">
+						<label class="<?=$labelClass?>" for="password"><span data-i18n="login_password"></span></label>
+						<input class="<?=$inputClass?>"  type="password" name="password" id="password" autocomplete="on" required>
+						<span toggle="#password" class="absolute w-10 h-10 bottom-0 right-0 cursor-pointer text-brand-1 flex items-center justify-center password-toggle <?=$config['icons']['password_visibility']?>"></span>
+					</div>
+
+					<!-- btn -->
+					<div class="mt-6">
+						<input class="<?=$btnClass?>" type="submit" name="submit" value="Login">
+					</div>
+					<?php if ($error !== false) {
+						echo '<span class="w-full flex mt-6 text-red-500" data-i18n="login_invalid"></span>'; 
+					} ?>  
+				</form>
+			</div>
+			<div class="w-full max-w-xl my-12 border-b border-solid border-white border-opacity-20"></div>
+		<?php endif; ?>
+
+
+		<div class="w-full max-w-xl rounded-lg py-8 bg-white flex flex-col shadow-xl relative">
+
+			<div class="px-4">
+				<h1 class="text-2xl font-bold text-center mb-6 border-solid border-b border-gray-200 pb-4 text-brand-1">Menü</h1>
+			</div>
+
+			<?php if(!$config['protect']['index'] || (!$config['protect']['localhost_index'] && $_SERVER['REMOTE_ADDR'] === $_SERVER['SERVER_ADDR']) || !$config['login']['enabled'] || (isset($_SESSION['auth']) && $_SESSION['auth'] === true)): ?>
+			<div class="w-12 h-12 bg-white absolute right-4 top-4 rounded-b-l-lg shadow-xls flex items-center justify-center text-brand-1 cursor-pointer">
+				<a href="/" >
+					<i class="text-xl <?php echo $config['icons']['close']; ?>"></i>
+					<!-- <span data-i18n="close"></span> -->
+				</a>
+			</div>
+			<?php endif; ?>
+
+			<div class="grid grid-cols-1 md:grid-cols-2 gap-4 px-4 ">
+				<?php 
+					if(!$config['protect']['admin'] || (!$config['protect']['localhost_admin'] && $_SERVER['REMOTE_ADDR'] === $_SERVER['SERVER_ADDR']) || !$config['login']['enabled'] || (isset($_SESSION['auth']) && $_SESSION['auth'] === true)) {
+						echo getMenuBtn("/admin", "admin_panel", $config['icons']['admin']);
+					}
+
+					echo getMenuBtn("/gallery.php", "gallery", $config['icons']['gallery']);
+					echo getMenuBtn("/slideshow", "slideshow", $config['icons']['slideshow']);
+
+					if(!$config['protect']['index'] || (!$config['protect']['localhost_index'] && $_SERVER['REMOTE_ADDR'] === $_SERVER['SERVER_ADDR']) || !$config['login']['enabled'] || (isset($_SESSION['auth']) && $_SESSION['auth'] === true)) {
+						echo getMenuBtn("/livechroma.php", "livechroma", $config['icons']['livechroma']);
+					}
+
+					if(!$config['protect']['manual'] || (!$config['protect']['localhost_manual'] && $_SERVER['REMOTE_ADDR'] === $_SERVER['SERVER_ADDR']) || !$config['login']['enabled'] || (isset($_SESSION['auth']) && $_SESSION['auth'] === true)) {
+						echo getMenuBtn("/faq", "show_faq", $config['icons']['faq']);
+						echo getMenuBtn("/manual", "show_manual", $config['icons']['manual']);
+						echo getMenuBtn("https://t.me/PhotoboothGroup", "telegram", $config['icons']['telegram']);
+					}
+
+					// echo getMenuBtn("/", "reload", $config['icons']['refresh']);
+
+					if(isset($_SESSION['auth']) && $_SESSION['auth'] === true) {
+						echo getMenuBtn("/login/logout.php", "logout", $config['icons']['logout']);
+					}
+
+				?>
+			</div>
+
+		</div>
+		
 		</div>
 	</div>
 
 	<script src="../node_modules/whatwg-fetch/dist/fetch.umd.js"></script>
-	<script type="text/javascript" src="../api/config.php?v=<?php echo $config['photobooth']['version']; ?>"></script>
+	<script type="text/javascript" src="../api/config.php?v=<?=$config['photobooth']['version']?>"></script>
 	<script type="text/javascript" src="../node_modules/jquery/dist/jquery.min.js"></script>
-	<script type="text/javascript" src="../resources/js/adminshortcut.js?v=<?php echo $config['photobooth']['version']; ?>"></script>
-	<script type="text/javascript" src="../resources/js/login.js?v=<?php echo $config['photobooth']['version']; ?>"></script>
-	<script type="text/javascript" src="../resources/js/theme.js?v=<?php echo $config['photobooth']['version']; ?>"></script>
+	<script type="text/javascript" src="../resources/js/adminshortcut.js?v=<?=$config['photobooth']['version']?>"></script>
+	<script type="text/javascript" src="../resources/js/login.js?v=<?=$config['photobooth']['version']?>"></script>
+	<script type="text/javascript" src="../resources/js/theme.js?v=<?=$config['photobooth']['version']?>"></script>
 	<script src="../node_modules/@andreasremdt/simple-translator/dist/umd/translator.min.js"></script>
-	<script type="text/javascript" src="../resources/js/i18n.js?v=<?php echo $config['photobooth']['version']; ?>"></script>
+	<script type="text/javascript" src="../resources/js/i18n.js?v=<?=$config['photobooth']['version']?>"></script>
 </body>
 </html>
