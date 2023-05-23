95390c23021a53acb550fe841b9b276fea779d7e
Andreas Blaesius <andi@unlegacy-android.org>
2023-05-23 22:56:18 +0200

    run yarn format
    Change-Id: I77c5b1e41d41ad2b69493ee93acbdf1379d0ef31

--- src/js/admin/navi.js
+++ src/js/admin/navi.js
@@ -1,48 +1,44 @@
 $(function () {
-
     // init navi item
     getInitialNaviItem();
     initDebugcontent();
 
     // item click
     $('.navItem').on('click', function () {
-        setNaviItem($(this)[0].id.replace('nav-', '') );
+        setNaviItem($(this)[0].id.replace('nav-', ''));
     });
-    
 });
 
-function isElementInViewport (el) {
+function isElementInViewport(el) {
     var offset = 40;
-    if( $('#activeTabLabel').is(':visible') ) {
+    if ($('#activeTabLabel').is(':visible')) {
         offset = 100;
     }
-    if (typeof jQuery === "function" && el instanceof jQuery) {
+    if (typeof jQuery === 'function' && el instanceof jQuery) {
         el = el[0];
     }
     var rect = el.getBoundingClientRect();
     var isActive = rect.top <= offset && rect.top >= -el.clientHeight;
 
-    if(isActive){
-        return (
-            true
-        );
+    if (isActive) {
+        return true;
     }
 }
-$(".adminContent").on("scroll", function() {
-    $(".adminSection.visible").each(function (idx, el) {
-        if ( isElementInViewport(el) ) {
-            var hash = window.location.hash; 
-            var urlHash = "#" + el.id;
-            if( hash != urlHash ) {
+$('.adminContent').on('scroll', function () {
+    $('.adminSection.visible').each(function (idx, el) {
+        if (isElementInViewport(el)) {
+            var hash = window.location.hash;
+            var urlHash = '#' + el.id;
+            if (hash != urlHash) {
                 window.history.pushState(null, null, urlHash);
-                setNaviItem( el.id );
+                setNaviItem(el.id);
             }
         }
     });
 });
 
 function getInitialNaviItem() {
-    if (!$('.navItem')[0]){
+    if (!$('.navItem')[0]) {
         return;
     }
     var urlHash = window.location.hash;
@@ -54,45 +50,45 @@ function getInitialNaviItem() {
         $('.navItem').removeClass('isActive');
         $('.navItem').first().addClass('isActive');
         var itemName = $('.navItem.isActive').find('.naviLabel').html();
-        $( '#activeTabLabel').html( itemName );
+        $('#activeTabLabel').html(itemName);
     }
 }
 
-function setNaviItem( item ) {
+function setNaviItem(item) {
     $('.navItem').removeClass('isActive');
     $('#nav-' + item).addClass('isActive');
 
     var itemName = $('.navItem.isActive').find('.naviLabel').html();
-    $( '#activeTabLabel').html( itemName );
+    $('#activeTabLabel').html(itemName);
 
     var top = $('#nav-' + item).offset().top;
     var height = $(window).height();
 
-    if( top <= height || top >= height ) {
-        $('#nav-' + item).parents()[0].scrollIntoView({ block: "end" });
+    if (top <= height || top >= height) {
+        $('#nav-' + item)
+            .parents()[0]
+            .scrollIntoView({block: 'end'});
     }
-
 }
 
 function toggleAdminNavi() {
-    if( $('.adminNavi').hasClass('isActive') ) {
+    if ($('.adminNavi').hasClass('isActive')) {
         $('.adminNavi').removeClass('isActive');
     } else {
         $('.adminNavi').addClass('isActive');
     }
 }
 
-function setDebugNavItemActive( elem ) {
-    $( '.debugNavItem' ).removeClass('isActive');
-    $( elem ).addClass('isActive');
+function setDebugNavItemActive(elem) {
+    $('.debugNavItem').removeClass('isActive');
+    $(elem).addClass('isActive');
     $('.adminNavi').removeClass('isActive');
 }
 
-
 function initDebugcontent() {
-    if (!$('.debugNavItem')[0]){
+    if (!$('.debugNavItem')[0]) {
         return;
     } else {
-        $('.debugNavItem').first().trigger( "click" );
+        $('.debugNavItem').first().trigger('click');
     }
 }
