e873079c65942105c8b9fce5fc0102a0905bfc16
Andreas Blaesius <andi@unlegacy-android.org>
2023-05-23 11:21:01 +0200

    admin: run yarn format

--- admin/helper/index.php
+++ admin/helper/index.php
@@ -1,8 +1,8 @@
 <?php
-    include("loader.php");
-    include("hiddenElement.php");
-    include("indent.php");
-    include("ctaBtn.php");
-    include("menuBtn.php");
-    include("toast.php");
+include 'loader.php';
+include 'hiddenElement.php';
+include 'indent.php';
+include 'ctaBtn.php';
+include 'menuBtn.php';
+include 'toast.php';
 ?>
