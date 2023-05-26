4caf092be2239b3280dfeab549f5488c8e145003
Andreas Blaesius <andi@unlegacy-android.org>
2023-05-26 07:08:43 +0200

    lib(log): prettier log, add docblocks

--- lib/log.php
+++ lib/log.php
@@ -2,21 +2,46 @@
 require_once __DIR__ . '/config.php';
 
 class DataLogger {
+    /** @var string The path to the log file. */
     public $logFile;
+
+    /** @var array The data to be logged. */
     public $logData = [];
 
+    /**
+     * DataLogger constructor.
+     *
+     * @param string $logFile The path to the log file.
+     */
     public function __construct($logFile) {
         $this->logFile = $logFile;
     }
 
+    /**
+     * Adds data to the log data array.
+     *
+     * @param mixed $data The data to be added to the log.
+     * @return void
+     */
     public function addLogData($data) {
         $this->logData[] = $data;
     }
 
+    /**
+     * Retrieves the log data.
+     *
+     * @return array The log data.
+     */
     public function getLogData() {
         return $this->logData;
     }
 
+    /**
+     * Logs data to a file.
+     *
+     * @param array|null $data The data to log. If null, retrieves the log data from the instance.
+     * @return void
+     */
     public function logToFile($data = null) {
         if ($data === null) {
             $data = $this->getLogData();
@@ -25,25 +50,7 @@ class DataLogger {
         $logFile = $this->logFile;
         $fileData = date('Y-m-d H:i:s') . ':' . PHP_EOL;
 
-        foreach ($data as $entry) {
-            if (is_array($entry)) {
-                $formattedEntry = '';
-                foreach ($entry as $key => $value) {
-                    if (is_array($value)) {
-                        $formattedValue = implode(', ', $value);
-                    } elseif (is_bool($value)) {
-                        $formattedValue = $value ? 'true' : 'false';
-                    } else {
-                        $formattedValue = $value;
-                    }
-                    $formattedEntry .= "'$key' : $formattedValue, ";
-                }
-                $formattedEntry = rtrim($formattedEntry, ', ');
-                $fileData .= rtrim($formattedEntry) . PHP_EOL;
-            } else {
-                $fileData .= $entry . PHP_EOL;
-            }
-        }
+        $fileData .= self::formatLogData($data);
 
         if (is_file($logFile)) {
             $fileData .= file_get_contents($logFile);
@@ -53,6 +60,64 @@ class DataLogger {
         $this->logData = [];
     }
 
+    /**
+     * Format the log data.
+     *
+     * @param array $data The log data to format.
+     * @param int $indentLevel The current indentation level.
+     * @return string The formatted log data.
+     */
+    private static function formatLogData($data, $indentLevel = 0) {
+        $fileData = '';
+
+        foreach ($data as $entry) {
+            $formattedEntry = self::formatArrayEntry($entry, $indentLevel);
+            $fileData .= self::indent($formattedEntry, $indentLevel) . PHP_EOL;
+        }
+
+        return $fileData;
+    }
+
+    /**
+     * Format an array entry.
+     *
+     * @param array $entry The array entry to format.
+     * @param int $indentLevel The current indentation level.
+     * @return string The formatted array entry.
+     */
+    private static function formatArrayEntry($entry, $indentLevel) {
+        $formattedEntry = '';
+
+        foreach ($entry as $key => $value) {
+            if (is_array($value)) {
+                $formattedValue = self::formatArrayEntry($value, $indentLevel + 1);
+                $formattedEntry .= self::indent($formattedValue, $indentLevel + 1) . PHP_EOL;
+            } else {
+                $formattedEntry .= self::indent("'{$key}' : {$value}", $indentLevel + 1) . PHP_EOL;
+            }
+        }
+
+        return $formattedEntry;
+    }
+
+    /**
+     * Indent a string with spaces based on the indentation level.
+     *
+     * @param string $string The string to indent.
+     * @param int $indentLevel The indentation level.
+     * @return string The indented string.
+     */
+    private static function indent($string, $indentLevel) {
+        $indentation = str_repeat('    ', $indentLevel);
+        return $indentation . $string;
+    }
+
+    /**
+     * Logs an error message and terminates the script execution.
+     *
+     * @param string $errormsg The error message to log.
+     * @return void
+     */
     public function logErrorAndDie($errormsg) {
         try {
             $errorData = [
