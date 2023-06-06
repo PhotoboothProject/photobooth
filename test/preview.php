9f26065091ba08e27202b0df11cc196e3d0d8c27
Andreas Blaesius <andi@unlegacy-android.org>
2023-06-06 16:07:00 +0200

    test: use header template
    Change-Id: I23fa088352d15d15ad8a2d8a40148d8acf986701

--- test/preview.php
+++ test/preview.php
@@ -4,34 +4,17 @@ require_once '../lib/config.php';
 $btnClass = 'btn btn--' . $config['ui']['button'];
 $btnShape = 'shape--' . $config['ui']['button'];
 $uiShape = 'shape--' . $config['ui']['style'];
+
+$fileRoot = '../';
+$pageTitle = $config['ui']['branding'] . ' Preview-Test';
 ?>
 
 <!DOCTYPE html>
 <html>
 
 <head>
-    <meta charset="UTF-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no">
-    <meta name="msapplication-TileColor" content="<?= $config['colors']['primary'] ?>">
-    <meta name="theme-color" content="<?= $config['colors']['primary'] ?>">
+	<?php include('../template/components/mainHead.php'); ?>
 
-    <title><?= $config['ui']['branding'] ?> Preview-Test</title>
-
-    <!-- Favicon + Android/iPhone Icons -->
-    <link rel="apple-touch-icon" sizes="180x180" href="../resources/img/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="../resources/img/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="../resources/img/favicon-16x16.png">
-    <link rel="manifest" href="../resources/img/site.webmanifest">
-    <link rel="mask-icon" href="../resources/img/safari-pinned-tab.svg" color="#5bbad5">
-
-    <!-- Fullscreen Mode on old iOS-Devices when starting photobooth from homescreen -->
-    <meta name="apple-mobile-web-app-capable" content="yes"/>
-    <meta name="apple-mobile-web-app-status-bar-style" content="black"/>
-
-    <link rel="stylesheet" href="../node_modules/normalize.css/normalize.css"/>
-    <link rel="stylesheet" href="../node_modules/font-awesome/css/font-awesome.css"/>
-    <link rel="stylesheet" href="../node_modules/material-icons/iconfont/material-icons.css">
-    <link rel="stylesheet" href="../node_modules/material-icons/css/material-icons.css">
     <link rel="stylesheet" href="../resources/css/test_preview.css?v=<?php echo $config['photobooth']['version']; ?>"/>
     <?php if (is_file('../private/overrides.css')): ?>
         <link rel="stylesheet" href="../private/overrides.css?v=<?php echo $config['photobooth']['version']; ?>"/>
