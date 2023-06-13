38edaf764f813ba0c7ffdd4069176799be9bfe17
Andreas Blaesius <andi@unlegacy-android.org>
2023-06-13 07:47:07 +0200

    core: only clear/reset timeout on keyup if timeout is pending

--- src/js/core.js
+++ src/js/core.js
@@ -1285,12 +1285,15 @@ const photoBooth = (function () {
     };
 
     $(document).on('keyup', function (ev) {
-        if (typeof onStandaloneGalleryView !== 'undefined' || startPage.is(':visible')) {
-            clearTimeout(timeOut);
-            photoboothTools.console.logDev('Timeout for auto reload cleared.');
-        } else {
-            api.resetTimeOut();
+        if (api.isTimeOutPending()) {
+            if (typeof onStandaloneGalleryView !== 'undefined' || startPage.is(':visible')) {
+                clearTimeout(timeOut);
+                photoboothTools.console.logDev('Timeout for auto reload cleared.');
+            } else {
+                api.resetTimeOut();
+            }
         }
+
         if (typeof onStandaloneGalleryView === 'undefined' && typeof onCaptureChromaView === 'undefined') {
             if (
                 (config.picture.key && parseInt(config.picture.key, 10) === ev.keyCode) ||
