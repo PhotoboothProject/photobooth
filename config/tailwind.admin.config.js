e6f6977281b96f1e5ed51e0b23e28b0e6889c287
Andreas Blaesius <andi@unlegacy-android.org>
2023-06-23 06:39:35 +0200

    Add new config for primary light color, change some default colors
    Change-Id: I5e8b5928273bd475ef2c8b861a8d6143135767e9

--- config/tailwind.admin.config.js
+++ config/tailwind.admin.config.js
@@ -13,6 +13,7 @@ module.exports = {
         'brand-1': 'var(--brand-1, #1B3FAA)',
         'brand-1-hover': 'var(--brand-1-hover, #153288)',
         'content-1': 'var(--content-1, #F1F5F8)',
+        'brand-2': 'var(--brand-2, #E8EBF6)',
       },
       boxShadow: {
         "xl": "0 3px 20px #0000001c"
