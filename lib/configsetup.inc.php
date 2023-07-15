wikijm/photobooth-2

Added Nextcloud Login Flow v2 Authentication support
Change-Id: I9c063c47f8425bcbe71ff1d64cc61e618d5bb44a

lib/configsetup.inc.php
@@ -3000,7 +3000,7 @@ $configsetup = [
             'value' => $config['nextcloud']['logfile'],
         ],
         'nextcloud_fileshare' => [
-            'view' => 'advanced',
+            'view' => 'expert',
             'type' => 'checkbox',
             'name' => 'nextcloud[fileshare]',
             'value' => $config['nextcloud']['fileshare'],
@@ -3032,16 +3032,23 @@ $configsetup = [
             'name' => 'nextcloud[mnt]',
             'value' => htmlentities($config['nextcloud']['mnt'] ?? ''),
         ],
+        'nextcloud_login' => [
+            'view' => 'advanced',
+            'type' => 'button',
+            'placeholder' => 'Login',
+            'name' => 'NCLOGINBUTTON',
+            'value' => 'nclogin-btn',
+        ],
         'nextcloud_user' => [
             'view' => 'advanced',
-            'type' => 'input',
+            'type' => 'hidden',
             'placeholder' => $defaultConfig['nextcloud']['user'],
             'name' => 'nextcloud[user]',
             'value' => $config['nextcloud']['user'],
         ],
         'nextcloud_pass' => [
             'view' => 'advanced',
-            'type' => 'input',
+            'type' => 'hidden',
             'placeholder' => $defaultConfig['nextcloud']['pass'],
             'name' => 'nextcloud[pass]',
             'value' => htmlentities($config['nextcloud']['pass'] ?? ''),

PhotoboothProject/photobooth

lib/configsetup.inc.php
@@ -3063,7 +3063,7 @@ $configsetup = [
             'value' => $config['nextcloud']['logfile'],
         ],
         'nextcloud_fileshare' => [
-            'view' => 'advanced',
+            'view' => 'expert',
             'type' => 'checkbox',
             'name' => 'nextcloud[fileshare]',
             'value' => $config['nextcloud']['fileshare'],
@@ -3095,16 +3095,23 @@ $configsetup = [
             'name' => 'nextcloud[mnt]',
             'value' => htmlentities($config['nextcloud']['mnt'] ?? ''),
         ],
+        'nextcloud_login' => [
+            'view' => 'advanced',
+            'type' => 'button',
+            'placeholder' => 'authentication',
+            'name' => 'NCLOGINBUTTON',
+            'value' => 'nclogin-btn',
+        ],
         'nextcloud_user' => [
             'view' => 'advanced',
-            'type' => 'input',
+            'type' => 'hidden',
             'placeholder' => $defaultConfig['nextcloud']['user'],
             'name' => 'nextcloud[user]',
             'value' => $config['nextcloud']['user'],
         ],
         'nextcloud_pass' => [
             'view' => 'advanced',
-            'type' => 'input',
+            'type' => 'hidden',
             'placeholder' => $defaultConfig['nextcloud']['pass'],
             'name' => 'nextcloud[pass]',
             'value' => htmlentities($config['nextcloud']['pass'] ?? ''),