4e6ae0cfd820733587c02d5c54959825ac0d8428
Andreas Blaesius <andi@unlegacy-android.org>
2023-07-18 08:43:16 +0200

    lib: add health checker
    Change-Id: I2ef35740fba9bde4cad161e7c426856fe021dd9c

---
+++ lib/healthcheck.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Class HealthCheck
+ *
+ * Performs a health check on the PHP environment.
+ */
+class HealthCheck {
+    /**
+     * @var bool Indicates the health status of the PHP environment.
+     */
+    public $healthStatus = false;
+
+    /**
+     * @var int The major version number of PHP.
+     */
+    public $phpMajor = 0;
+
+    /**
+     * @var int The minor version number of PHP.
+     */
+    public $phpMinor = 0;
+
+    /**
+     * @var bool Indicates whether the GD extension is enabled.
+     */
+    public $gdEnabled = false;
+
+    /**
+     * @var bool Indicates whether the ZIP extension is enabled.
+     */
+    public $zipEnabled = false;
+
+    /**
+     * HealthCheck constructor.
+     *
+     * Initializes the HealthCheck object and performs the health check.
+     */
+    function __construct() {
+        list($this->phpMajor, $this->phpMinor) = $this->phpVersion();
+        $this->gdEnabled = extension_loaded('gd');
+        $this->zipEnabled = extension_loaded('zip');
+        if ($this->phpMajor >= 8 && $this->gdEnabled && $this->zipEnabled) {
+            $this->healthStatus = true;
+        }
+    }
+
+    /**
+     * Retrieves the major and minor versions of PHP.
+     *
+     * @return int[] An array containing the major and minor versions of PHP.
+     */
+    public static function phpVersion() {
+        try {
+            return [\PHP_MAJOR_VERSION, \PHP_MINOR_VERSION];
+        } catch (Exception $e) {
+            return [0, 0];
+        }
+    }
+}
