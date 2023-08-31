1c0f87aa2fbc1266b4c8a2bb7f472e072315cc78
Andreas Blaesius <andi@unlegacy-android.org>
2023-08-31 19:35:57 +0200

    build: fix for prettier on test/
    Change-Id: I7524cc65b4123ff27c3cf9a7444a31734a481a25

--- test/index.php
+++ test/index.php
@@ -1,15 +1,16 @@
 <?php
 $fileRoot = '../';
 
-require_once($fileRoot . 'lib/config.php');
+require_once $fileRoot . 'lib/config.php';
 
 $pageTitle = 'Tests';
-include($fileRoot . 'admin/components/head.admin.php');
-include($fileRoot . 'admin/helper/index.php');
+include $fileRoot . 'admin/components/head.admin.php';
+include $fileRoot . 'admin/helper/index.php';
 
 $labelClass = 'w-full flex flex-col mb-1';
 $inputClass = 'w-full h-10 border border-solid border-gray-300 focus:border-brand-1 rounded-md px-3 mt-auto';
-$btnClass = 'w-full h-12 rounded-full bg-brand-1 text-white flex items-center justify-center relative ml-auto border-2 border-solid border-brand-1 hover:bg-white hover:text-brand-1 transition font-bold px-4';
+$btnClass =
+    'w-full h-12 rounded-full bg-brand-1 text-white flex items-center justify-center relative ml-auto border-2 border-solid border-brand-1 hover:bg-white hover:text-brand-1 transition font-bold px-4';
 ?>
 
 <body>
@@ -22,20 +23,20 @@ $btnClass = 'w-full h-12 rounded-full bg-brand-1 text-white flex items-center ju
 					</h1>
 				</div>
 				<div class="w-12 h-12 bg-white absolute right-4 top-4 rounded-b-l-lg shadow-xls flex items-center justify-center text-brand-1 cursor-pointer">
-					<a href="<?=$fileRoot . 'login'?>" >
+					<a href="<?= $fileRoot . 'login' ?>" >
 						<i class="!text-2xl <?php echo $config['icons']['close']; ?>"></i>
 					</a>
 				</div>
 
 				<div class="grid grid-cols-1 md:grid-cols-2 gap-4 px-4 ">
-				<?php 
-					echo getMenuBtn('../test/healthcheck.php', 'healthCheck', '');
-					echo getMenuBtn('../test/phpinfo.php', 'phpinfo', '');
-					echo getMenuBtn('../test/photo.php', 'pictureTest', '');
-					echo getMenuBtn('../test/collage.php', 'collageTest', '');
-					echo getMenuBtn('../test/preview.php', 'previewTest', '');
-					echo getMenuBtn('../test/chroma.php', 'chromaPreviewTest', '');
-					echo getMenuBtn('../test/trigger.php', 'remotebuzzerGetTrigger', '');
+				<?php
+				echo getMenuBtn('../test/healthcheck.php', 'healthCheck', '');
+				echo getMenuBtn('../test/phpinfo.php', 'phpinfo', '');
+				echo getMenuBtn('../test/photo.php', 'pictureTest', '');
+				echo getMenuBtn('../test/collage.php', 'collageTest', '');
+				echo getMenuBtn('../test/preview.php', 'previewTest', '');
+				echo getMenuBtn('../test/chroma.php', 'chromaPreviewTest', '');
+				echo getMenuBtn('../test/trigger.php', 'remotebuzzerGetTrigger', '');
 				?>
 				</div>
 
@@ -44,5 +45,5 @@ $btnClass = 'w-full h-12 rounded-full bg-brand-1 text-white flex items-center ju
 	</div>
 
 <?php
-include($fileRoot . 'admin/components/footer.admin.php');
+include $fileRoot . 'admin/components/footer.admin.php';
 ?>
