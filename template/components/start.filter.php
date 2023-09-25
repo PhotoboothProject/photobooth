ab0a4c3cda015f90301ae4208709c44780ef16e7
Benjamin Kott <benjamin.kott@outlook.com>
2023-09-25 08:34:29 +0200

    task: improve filternav rendering

--- template/components/start.filter.php
+++ template/components/start.filter.php
@@ -1,17 +1,31 @@
 <?php
 
 use Photobooth\Enum\ImageFilterEnum;
+use Photobooth\Service\LanguageService;
+
+$languageService = LanguageService::getInstance();
 
 ?>
-<div id="mySidenav" class="dragscroll sidenav rotarygroup">
-    <a href="#" class="<?php echo $btnClass; ?> closebtn rotaryfocus"><i class="<?php echo $config['icons']['close']; ?>"></i></a>
-    <?php foreach (ImageFilterEnum::cases() as $filter): ?>
-        <?php if (!in_array($filter->value, $config['filters']['disabled'])): ?>
-            <div id="<?= $filter->value ?>" class="filter <?php if ($config['filters']['defaults'] === $filter->value) {
-                echo 'activeSidenavBtn';
-            } ?>">
-                <a class="btn btn--small rotaryfocus" href="#"><?= $filter->label() ?></a>
-            </div>
-        <?php endif; ?>
-    <?php endforeach; ?>
+<div id="filternav" class="dragscroll sidenav rotarygroup">
+    <button
+        type="button"
+        class="sidenav-close rotaryfocus"
+        data-command="sidenav-close"
+        title="<?=$languageService->translate('close')?>"
+        >
+        <i class="<?php echo $config['icons']['close']; ?>"></i>
+    </button>
+    <div class="sidenav-list">
+        <?php foreach (ImageFilterEnum::cases() as $filter): ?>
+            <?php if (!in_array($filter->value, $config['filters']['disabled'])): ?>
+                <button
+                    type="button"
+                    class="sidenav-list-item<?php echo ImageFilterEnum::tryFrom($config['filters']['defaults']) === $filter ? ' sidenav-list-item--active' : ''; ?> rotaryfocus"
+                    data-filter="<?= $filter->value ?>"
+                >
+                    <?= $filter->label() ?>
+                </button>
+            <?php endif; ?>
+        <?php endforeach; ?>
+    </div>
 </div>
