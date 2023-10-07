21eb3f735d20cd4422223ac5ef7c44e1f1205ab5
Benjamin Kott <benjamin.kott@outlook.com>
2023-10-07 14:49:56 +0200

    task: modernize stages

--- template/components/start.loader.php
+++ template/components/start.loader.php
@@ -1,12 +1,13 @@
 
 <div class="stage rotarygroup" data-stage="loader">
-    <div class="loaderInner">
+    <div class="stage-inner">
         <div class="spinner">
             <i class="<?php echo $config['icons']['spinner']; ?>"></i>
         </div>
         <div id="ipcam--view" class="<?php echo $config['preview']['style']; ?>"></div>
         <canvas id="video--sensor"></canvas>
-        <div class="loaderImage"></div>
-        <div class="loading rotarygroup"></div>
+        <div class="stage-image"></div>
+        <div class="stage-message"></div>
+        <div class="buttonbar buttonbar--bottom"></div>
     </div>
 </div>
