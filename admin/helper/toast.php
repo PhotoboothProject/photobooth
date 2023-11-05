3b74f7ae3ff27d87063e56a746c006082925d030
Benjamin Kott <benjamin.kott@outlook.com>
2023-11-05 10:07:49 +0100

    task: drop unused params from toast function

--- admin/helper/toast.php
+++ admin/helper/toast.php
@@ -1,24 +1,20 @@
 <?php
 
-function getToast($type = false, $msg = false)
+function getToast()
 {
     $transition = 'slideInRight';
     $scheme = '
-            bg-white
-            [&.isSuccess]:bg-green-500 [&.isSuccess]:font-bold [&.isSuccess]:text-white
-            [&.isError]:bg-red-500 [&.isError]:font-bold [&.isError]:text-white
-        ';
+        bg-white
+        [&.isSuccess]:bg-green-500 [&.isSuccess]:font-bold [&.isSuccess]:text-white
+        [&.isError]:bg-red-500 [&.isError]:font-bold [&.isError]:text-white
+    ';
 
     return '
-                <div class="adminToast hidden [&.isActive]:flex w-64 p-4 rounded-md shadow-md top-2 right-2 fixed ' .
-        $scheme .
-        ' ' .
-        $transition .
-        '">
-                    <div class="flex items-start">
-                        <div class="adminToast-icon fa fa-check mr-3 mt-1 text-white"></div>
-                        <div class="headline">Erfolgreich</div>
-                    </div>
-                </div>
-            ';
+        <div class="adminToast hidden [&.isActive]:flex w-64 p-4 rounded-md shadow-md top-2 right-2 fixed ' . $scheme . ' ' . $transition . '">
+            <div class="flex items-start">
+                <div class="adminToast-icon fa fa-check mr-3 mt-1 text-white"></div>
+                <div class="headline">Erfolgreich</div>
+            </div>
+        </div>
+    ';
 }
