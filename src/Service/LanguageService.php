d580d8faa69fbefcc5d600acfecaa30f85205715
Benjamin Kott <benjamin.kott@outlook.com>
2023-12-23 12:47:23 +0100

    bugfix: correct loading of language files

--- src/Service/LanguageService.php
+++ src/Service/LanguageService.php
@@ -22,7 +22,7 @@ class LanguageService
         $path = PathUtility::getAbsolutePath($localeFolder);
         if (PathUtility::isAbsolutePath($path)) {
             foreach (new \DirectoryIterator($path) as $file) {
-                if(!$file->isFile() || strtolower($file->getExtension() !== 'json')) {
+                if(!$file->isFile() || strtolower($file->getExtension()) !== 'json') {
                     continue;
                 }
                 $translator->addResource('json', $path . '/' . $file->getFilename(), $file->getBasename('.' . $file->getExtension()), 'photobooth');
