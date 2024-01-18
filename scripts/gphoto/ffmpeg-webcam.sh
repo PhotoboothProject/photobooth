d12751855e2be6bf947e2ae748fe515b22aa02b2
dadav <33197631+dadav@users.noreply.github.com>
2024-01-18 20:08:51 +0100

    feat: Improve scripts

--- scripts/gphoto/ffmpeg-webcam.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# setup ffmpeg webcam
-modprobe v4l2loopback exclusive_caps=1 card_label="GPhoto2 Webcam"
-# disable any other webcam but ignore if it exits with an error because there was no other webcam
-rmmod bcm2835-isp || true
