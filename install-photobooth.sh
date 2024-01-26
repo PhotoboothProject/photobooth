e8b764aa7cd506cb70d262330faabc3270d14db4
Andreas Blaesius <andi@unlegacy-android.org>
2024-01-26 21:44:58 +0100

    install-photobooth: fix defaults on help

--- install-photobooth.sh
+++ install-photobooth.sh
@@ -153,7 +153,7 @@ function no_raspberry {
 
 function view_help() {
     cat <<EOF
-Usage: sudo bash install-photobooth.sh -u=<YourUsername> [-b=<stable3:dev:package> -hprsV -w=<apache:nginx:lighttpd]
+Usage: sudo bash install-photobooth.sh -u=<YourUsername> [-b=<stable4:dev:package> -hprsV -w=<apache:nginx:lighttpd]
 
     -h,  -help,       --help        Display help.
 
@@ -162,7 +162,7 @@ Usage: sudo bash install-photobooth.sh -u=<YourUsername> [-b=<stable3:dev:packag
                                     By default, latest development verison (dev) will be installed.
                                     package will install latest Release from zip.
 
-    -p,  -php,        --php         Choos the PHP version to install (Default = 7.4)
+    -p,  -php,        --php         Choos the PHP version to install (Default = 8.2)
 
     -r,  -raspberry,  --raspberry   Skip Pi detection and add Pi specific adjustments.
                                     Note: only to use on Raspberry Pi OS!
