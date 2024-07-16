f71646504ad261862bb6c558ec0d3e42695c94ac
Francesco Miccolis <miccolis9872@gmail.com>
2024-07-16 18:11:05 +0200

    started fix to file upload

--- admin/upload/index.php
+++ admin/upload/index.php
@@ -6,6 +6,7 @@ use Photobooth\Service\ApplicationService;
 use Photobooth\Service\LanguageService;
 use Photobooth\Utility\ImageUtility;
 use Photobooth\Utility\FontUtility;
+use Photobooth\Utility\FileUtility;
 use Photobooth\Utility\PathUtility;
 use Photobooth\Service\LoggerService;
 
@@ -34,6 +35,10 @@ $folderName = null;
 $labelClass = 'w-full flex flex-col mb-1';
 $inputClass = 'w-full h-10 border border-solid border-gray-300 focus:border-brand-1 rounded-md px-3 mb-3 mt-auto';
 $btnClass = 'w-full h-12 rounded-full bg-brand-1 text-white flex items-center justify-center relative ml-auto border-2 border-solid border-brand-1 hover:bg-white hover:text-brand-1 transition font-bold px-4';
+
+if ( !isset($_SERVER['HTACCESS']) ) {
+    $errors['htaccess'] = 'no-htaccess';
+}
 
 if (isset($_POST['submit'])) {
     $folderName = $_POST['folder_name'];
@@ -65,29 +70,35 @@ if (isset($_POST['submit'])) {
 
     if (count($errors) === 0) {
         for ($i = 0; $i < count($uploadedFiles['name']); $i++) {
-            $fileErrors = [];
+            $fileError = $uploadedFiles['error'][$i];
             $fileName = $uploadedFiles['name'][$i];
-            $fileTmpName = $uploadedFiles['tmp_name'][$i];
-            $fileType = $uploadedFiles['type'][$i];
-            $filePath = $folderPath . '/' . $fileName;
-            $logger->debug('fileName', [$fileName]);
-
-            // check if filetype is allowed for this folder
-            if(!in_array($fileType, $typeChecker[$folderName])) {
-                $errors[$fileName] = $languageService->translate('upload_wrong_type');
-                $logger->debug($languageService->translate('upload_wrong_type'), [$fileName]);
-                break;
+            if($fileError == 0) {
+                $fileTmpName = $uploadedFiles['tmp_name'][$i];
+                $fileType = $uploadedFiles['type'][$i];
+                $fileSize = $uploadedFiles['size'][$i];
+                $filePath = $folderPath . '/' . $fileName;
+                $logger->debug('fileName', [$fileName]);
+    
+                // check if filetype is allowed for this folder
+                if(!in_array($fileType, $typeChecker[$folderName])) {
+                    $errors[$fileName] = $languageService->translate('upload_wrong_type');
+                    $logger->debug($languageService->translate('upload_wrong_type'), [$fileName]);
+                    break;
+                }
+    
+                // check if file already exists
+                if(file_exists($filePath)) {
+                    $errors[$fileName] = $languageService->translate('upload_file_already_exists');
+                    $logger->debug($languageService->translate('upload_file_already_exists'), [$fileName]);
+                    break;
+                }
+    
+                move_uploaded_file($fileTmpName, $filePath);
+                chmod($filePath, 0644);
+            } else {
+                $errors[$fileName] = $languageService->translate(FileUtility::getErrorMessage($fileError));
+                $logger->debug($languageService->translate(FileUtility::getErrorMessage($fileError)), [$fileName]);
             }
-
-            // check if file already exists
-            if(file_exists($filePath)) {
-                $errors[$fileName] = $languageService->translate('upload_file_already_exists');
-                $logger->debug($languageService->translate('upload_file_already_exists'), [$fileName]);
-                break;
-            }
-
-            move_uploaded_file($fileTmpName, $filePath);
-            chmod($filePath, 0644);
         }
     }
 
@@ -150,7 +161,6 @@ if (isset($_SESSION['auth']) && $_SESSION['auth'] === true) {
         </div>
     </div>
 </div>
-
 <?php
 
 include PathUtility::getAbsolutePath('admin/components/footer.scripts.php');
--- src/Utility/FileUtility.php
+++ src/Utility/FileUtility.php
@@ -6,6 +6,17 @@ class FileUtility
 {
     public const DIRECTORY_PERMISSIONS = 0755;
 
+    public const FILE_UPLOAD_ERROR_MESSAGES = array(
+        UPLOAD_ERR_OK => 'There is no error, the file uploaded with success',
+        UPLOAD_ERR_INI_SIZE => 'The uploaded file exceeds the upload_max_filesize directive in php.ini',
+        UPLOAD_ERR_FORM_SIZE => 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form',
+        UPLOAD_ERR_PARTIAL => 'The uploaded file was only partially uploaded',
+        UPLOAD_ERR_NO_FILE => 'No file was uploaded',
+        UPLOAD_ERR_NO_TMP_DIR => 'Missing a temporary folder',
+        UPLOAD_ERR_CANT_WRITE => 'Failed to write file to disk.',
+        UPLOAD_ERR_EXTENSION => 'A PHP extension stopped the file upload.',
+    );
+
     public static function createDirectory(string $directory): void
     {
         if (!file_exists($directory) && !is_dir($directory)) {
@@ -17,5 +28,10 @@ class FileUtility
                 throw new \Exception('Failed to change permissions of directory: ' . $directory);
             }
         }
+    }
+
+    public static function getErrorMessage(int $errorCode): string
+    {
+        return self::FILE_UPLOAD_ERROR_MESSAGES[$errorCode];
     }
 }
