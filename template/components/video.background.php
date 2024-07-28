cfb0db45a0a745eb801151f5fd0a00b2f729b10d
Francesco Miccolis <miccolis9872@gmail.com>
2024-07-28 12:07:56 +0200

    included youtube video and live support
    Change-Id: Ifd462b0aeb4f53e4f823cc0cbc0246aecf124964

--- template/components/video.background.php
+++ template/components/video.background.php
@@ -8,6 +8,10 @@ $videoPath = $config['background']['video'];
 
 if ($isVideoBackground) {
     echo '<div id="video-background">';
-    echo '<video autoplay muted loop playsinline src="' . $videoPath . '"></video>';
+    if (str_starts_with($videoPath, 'https://www.youtube.com/embed/')) {
+        echo '<iframe width="100%" height="100%" src="' . $videoPath . '?autoplay=1&mute=1&controls=0&modestbranding=1%rel=0" frameborder="0" allow="autoplay" allowfullscreen></iframe>';
+    } else {
+        echo '<video autoplay muted loop playsinline src="' . $videoPath . '"></video>';
+    }
     echo '</div>';
 }
