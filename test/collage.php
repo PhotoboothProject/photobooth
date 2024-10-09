7a93ef5ecec3505d0ad21790796daec736d4fe71
Andreas Skomski <andi@unlegacy-android.org>
2024-10-09 12:33:40 +0200

    test: prettier collage.php
    Change-Id: Ie2a94f77c70e94f8a3bc685dcbe3a56b68639c9e

--- test/collage.php
+++ test/collage.php
@@ -4,40 +4,82 @@ require_once '../lib/boot.php';
 
 use Photobooth\Collage;
 use Photobooth\Enum\FolderEnum;
-use Photobooth\Image;
 use Photobooth\Utility\ImageUtility;
+use Photobooth\Utility\PathUtility;
+use Photobooth\Service\ApplicationService;
+use Photobooth\Service\LanguageService;
+use Photobooth\Service\LoggerService;
 
-$demoImages = ImageUtility::getDemoImages($config['collage']['limit']);
+$loggerService = LoggerService::getInstance();
+$logger = $loggerService->getLogger('main');
+$logger->debug(basename($_SERVER['PHP_SELF']));
 
-$name = date('Ymd_His') . '.jpg';
-$collageSrcImagePaths = [];
-for ($i = 0; $i < $config['collage']['limit']; $i++) {
-    $image = $demoImages[$i];
-    $path = FolderEnum::TEMP->absolute() . DIRECTORY_SEPARATOR . $i . '_' . $name;
-    if (!copy($image, $path)) {
-        throw new \Exception('Failed to copy image.');
+$languageService = LanguageService::getInstance();
+$errorMessage = '';
+try {
+    $demoImages = ImageUtility::getDemoImages($config['collage']['limit']);
+
+    $name = date('Ymd_His') . '.jpg';
+    $collageSrcImagePaths = [];
+    for ($i = 0; $i < $config['collage']['limit']; $i++) {
+        $image = $demoImages[$i];
+        $path = FolderEnum::TEMP->absolute() . DIRECTORY_SEPARATOR . $i . '_' . $name;
+        if (!copy($image, $path)) {
+            throw new \Exception('Failed to copy image.');
+        }
+        $collageSrcImagePaths[] = $path;
     }
-    $collageSrcImagePaths[] = $path;
+
+    $filename_tmp = FolderEnum::TEMP->absolute() . DIRECTORY_SEPARATOR . 'result_' . $name;
+    if (Collage::createCollage($config, $collageSrcImagePaths, $filename_tmp, $config['filters']['defaults'])) {
+        for ($k = 0; $k < $config['collage']['limit']; $k++) {
+            unlink($collageSrcImagePaths[$k]);
+        }
+    }
+} catch (\Exception $e) {
+    $errorMessage = $e->getMessage();
+    $logger->error($errorMessage);
 }
 
-$filename_tmp = FolderEnum::TEMP->absolute() . DIRECTORY_SEPARATOR . 'result_' . $name;
-if (Collage::createCollage($config, $collageSrcImagePaths, $filename_tmp, $config['filters']['defaults'])) {
-    for ($k = 0; $k < $config['collage']['limit']; $k++) {
-        unlink($collageSrcImagePaths[$k]);
-    }
-    $imageHandler = new Image();
-    $imageHandler->debugLevel = $config['dev']['loglevel'];
-    $imageHandler->imageModified = false;
+$pageTitle = 'Collage test - ' . ApplicationService::getInstance()->getTitle();
+include PathUtility::getAbsolutePath('admin/components/head.admin.php');
+include PathUtility::getAbsolutePath('admin/helper/index.php');
+?>
 
-    $imageResource = $imageHandler->createFromImage($filename_tmp);
-    if (!$imageResource) {
-        throw new \Exception('Error creating image resource.');
-    }
+<div class="w-full h-screen grid place-items-center absolute bg-brand-2 px-6 py-12 overflow-x-hidden overflow-y-auto">
+    <div class="w-full flex items-center justify-center flex-col">
+        <div class="w-full max-w-xl h-144 rounded-lg p-8 bg-white flex flex-col shadow-xl">
+            <div class="w-full flex flex-col items-center justify-center text-2xl font-bold text-brand-1 mb-2">
+                Collage Test
+            </div>
+            <?php
+                    if (empty($errorMessage)) {
+                        echo '<div class="border border-solid border-black">';
+                        echo '<img src="' . PathUtility::getPublicPath($filename_tmp) . '" alt="Test Image">';
+                        echo '</div>';
+                    } else {
+                        echo '<div class="flex flex-col gap-2">';
+                        echo '<div class="flex flex-col justify-between p-2 rounded bg-red-300 text-red-800 border-2 border-red-800"><div class="col-span-1">' . $errorMessage . '</div></div>';
+                        echo '</div>';
+                    }
+?>
+        </div>
 
-    header('Content-Type: image/jpeg');
+        <div class="w-full max-w-xl my-12 border-b border-solid border-white border-opacity-20">
+        </div>
 
-    imagejpeg($imageResource);
-    unset($imageResource);
-    $imageHandler = null;
-    unlink($filename_tmp);
-}
+        <div class="w-full max-w-xl rounded-lg py-8 bg-white flex flex-col shadow-xl relative">
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-4 px-4 ">
+            <?php
+echo getMenuBtn(PathUtility::getPublicPath('admin'), 'admin_panel', $config['icons']['admin']);
+echo getMenuBtn(PathUtility::getPublicPath('test'), 'testMenu', $config['icons']['admin']);
+echo getMenuBtn(PathUtility::getPublicPath('admin/generator'), 'layout_generator', $config['icons']['take_collage']);
+?>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+include PathUtility::getAbsolutePath('admin/components/footer.scripts.php');
+include PathUtility::getAbsolutePath('admin/components/footer.admin.php');
