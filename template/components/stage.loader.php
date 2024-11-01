1835917e5baa867585e61cc92af32eea4affd638
Andreas Skomski <andi@unlegacy-android.org>
2024-11-01 23:09:13 +0100

    flip collage preview image depending on picture flip
    Change-Id: I4354fa291cc8f476ccd510f30ef6e06b36972557

--- template/components/stage.loader.php
+++ template/components/stage.loader.php
@@ -2,7 +2,7 @@
 <div class="stage stage--loader rotarygroup" data-stage="loader">
     <div class="stage-inner">
         <canvas id="video--sensor"></canvas>
-        <div class="stage-image"></div>
+        <div class="stage-image <?=$config['picture']['flip']?>"></div>
         <div class="stage-message"></div>
         <div class="buttonbar buttonbar--bottom"></div>
     </div>
