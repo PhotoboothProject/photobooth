26b39ebeb9600eecbfe2aa7e5361939a383704f2
Andreas Skomski <andi@unlegacy-android.org>
2025-09-24 09:47:14 +0200

    Update wgetcaptureconfig.php

--- admin/wgetcaptureconfig.php
+++ admin/wgetcaptureconfig.php
@@ -15,7 +15,7 @@ if (!(
     (!$config['protect']['localhost_admin'] && isset($_SERVER['SERVER_ADDR']) &&  $_SERVER['REMOTE_ADDR'] === $_SERVER['SERVER_ADDR']) ||
     (isset($_SESSION['auth']) && $_SESSION['auth'] === true) || !$config['protect']['admin']
 )) {
-    header('location: ' . PathUtility::getPublicPath('admin'));
+    header('location: ' . PathUtility::getPublicPath('login'));
     exit();
 }
 
