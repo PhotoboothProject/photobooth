a1f48cc339a1a1f974ac9874a7acff9e4a1d074e
reloxx13 <reloxx@interia.pl>
2025-11-26 18:47:31 +0100

    Chroma background selector robustness

--- template/components/chroma.background.selector.php
+++ template/components/chroma.background.selector.php
@@ -5,7 +5,12 @@ use Photobooth\Utility\PathUtility;
 
 echo '<div class="chroma-background-selector">';
 $backgroundsPath = $config['keying']['private_backgrounds'] ? 'private/images/keyingBackgrounds' : 'resources/img/background';
-$backgroundImages = ImageUtility::getImagesFromPath(PathUtility::getAbsolutePath($backgroundsPath));
+$backgroundImages = [];
+try {
+    $backgroundImages = ImageUtility::getImagesFromPath(PathUtility::getAbsolutePath($backgroundsPath));
+} catch (\Exception $e) {
+    // If no backgrounds are available, render nothing and keep UI usable.
+}
 foreach ($backgroundImages as $backgroundImage) {
     echo '<img src="' . PathUtility::getPublicPath($backgroundImage) . '" class="chroma-background-selector-image rotaryfocus" onclick="setChromaImage(this.src)">';
 }
