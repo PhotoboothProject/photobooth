bb8292e12936e8d6d66fe11483ce16365d78123c
reloxx13 <reloxx@interia.pl>
2025-12-30 13:21:58 +0100

    add collage limit calculator

--- api/applyEffects.php
+++ api/applyEffects.php
@@ -60,13 +60,10 @@ try {
             }
         }
     }
-    if (isset($_POST['collageLimit'])) {
-        $config['collage']['limit'] = $_POST['collageLimit'];
-        if ($config['collage']['placeholder'] && $config['collage']['limit'] < $config['collage']['placeholderposition']) {
-            $logger->debug('Placeholder position out of range. Limit: ' . $config['collage']['limit'] . ' Position: ' . $config['collage']['placeholderposition']);
-            $config['collage']['placeholderposition'] = 1;
-        }
-    }
+
+    $limitData = Collage::calculateLimit($config['collage'], $logger);
+    $config['collage']['limit'] = $limitData['limit'];
+    $config['collage']['placeholder'] = $limitData['placeholderEnabled'];
 
     $vars['style'] = $_POST['style'];
 
