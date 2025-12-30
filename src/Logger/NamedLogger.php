a1cdeb7e5ff2c415197eedbea486b4a809d26964
reloxx13 <reloxx@interia.pl>
2025-12-30 13:15:12 +0100

    logger use correct interface

--- src/Logger/NamedLogger.php
+++ src/Logger/NamedLogger.php
@@ -7,8 +7,9 @@ use Monolog\Handler\StreamHandler;
 use Monolog\Level;
 use Monolog\Logger;
 use Photobooth\Utility\PathUtility;
+use Psr\Log\LoggerInterface;
 
-class NamedLogger
+class NamedLogger implements LoggerInterface
 {
     protected int $level;
     protected string $file;
@@ -41,17 +42,47 @@ class NamedLogger
         $this->logger->pushHandler($stream);
     }
 
-    public function debug(string $message, array $context = []): void
+    public function debug(string|\Stringable $message, array $context = []): void
     {
         $this->logger->debug($message, $context);
     }
 
-    public function info(string $message, array $context = []): void
+    public function notice(string|\Stringable $message, array $context = []): void
+    {
+        $this->logger->notice($message, $context);
+    }
+
+    public function warning(string|\Stringable $message, array $context = []): void
+    {
+        $this->logger->warning($message, $context);
+    }
+
+    public function critical(string|\Stringable $message, array $context = []): void
+    {
+        $this->logger->critical($message, $context);
+    }
+
+    public function alert(string|\Stringable $message, array $context = []): void
+    {
+        $this->logger->alert($message, $context);
+    }
+
+    public function emergency(string|\Stringable $message, array $context = []): void
+    {
+        $this->logger->emergency($message, $context);
+    }
+
+    public function log($level, string|\Stringable $message, array $context = []): void
+    {
+        $this->logger->log($level, $message, $context);
+    }
+
+    public function info(string|\Stringable $message, array $context = []): void
     {
         $this->logger->info($message, $context);
     }
 
-    public function error(string $message, array $context = []): void
+    public function error(string|\Stringable $message, array $context = []): void
     {
         $this->logger->error($message, $context);
     }
